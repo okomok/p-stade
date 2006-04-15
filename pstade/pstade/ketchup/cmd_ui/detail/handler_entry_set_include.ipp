ff80fd6b6f1faf311d5ffa19637579d003961a1e
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2006-04-15 01:39:45 +0000

    Removed file/folder
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@19 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/pstade/ketchup/cmd_ui/detail/handler_entry_set_include.ipp
+++
@@ -1,4 +0,0 @@
-#include <pstade/unused.hpp>
-#include "../../cmd_ui/cmd_ui.hpp"
-#include "../../cmd_ui/dependent_value.hpp"
-#include "../../cmd_ui/update_cmd_ui_message.hpp"
--- pstade/pstade/vodka/boost/ptr_container_ptr_vector.hpp
+++
@@ -1,8 +0,0 @@
-
-
-#if defined(_MSC_VER)
-	#pragma warning(push)
-	#pragma warning(disable: 4127) // conditional expression is constant
-	#include <boost/ptr_container/ptr_vector.hpp>
-	#pragma warning(pop)
-#endif
