ede5f5bd8257a4b374d84bca38f73f815456843c
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2006-05-02 13:01:05 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@96 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/statement.hpp
@@ -0,0 +1,58 @@
+#ifndef PSTADE_STATEMENT_HPP
+#define PSTADE_STATEMENT_HPP
+
+
+// PStade.Wine
+//
+// Copyright MB 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/serialization/force_include.hpp> // BOOST_DLLEXPORT
+
+
+#define PSTADE_STATEMENT(Label, Statement) \
+    PSTADE_STATEMENT_define_initializer_type(Label, Statement) \
+    \
+    PSTADE_INSTANCE(PSTADE_STATEMENT_initializer_type(Label), \
+	    PSTADE_STATEMENT_initializer(Label), value) \
+    \
+    BOOST_DLLEXPORT \
+    PSTADE_STATEMENT_initializer_type(Label) * \
+    PSTADE_STATEMENT_function(Label)() \
+    { \
+        return &PSTADE_STATEMENT_initializer(Label); \
+    } \
+/**/
+
+
+#define PSTADE_STATEMENT_define_initializer_type(Label, Statement) \
+    struct PSTADE_STATEMENT_initializer_type(Label) \
+    { \
+        PSTADE_STATEMENT_initializer_type(Label)() \
+        { \
+            Statement \
+        } \
+    }; \
+/**/
+
+
+#define PSTADE_STATEMENT_initializer_type(Label) \
+    BOOST_PP_CAT(pstade_statement_initializer_type_of_, Label) \
+/**/
+
+
+#define PSTADE_STATEMENT_initializer(Label) \
+    BOOST_PP_CAT(pstade_statement_initializer_of_, Label) \
+/**/
+
+
+#define PSTADE_STATEMENT_function(Label) \
+    BOOST_PP_CAT(pstade_statement_function_of_, Label) \
+/**/
+
+
+#endif
