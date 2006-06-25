87bf1f6a9de54fec32b77b1e8b4458dcfa2fe458
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2006-06-25 20:50:28 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@155 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/libs/oven/test/zip_range.cpp
@@ -0,0 +1,67 @@
+#include <pstade/vodka/drink.hpp>
+#include <boost/test/minimal.hpp>
+
+
+// PStade.Oven
+//
+// Copyright MB 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <pstade/oven/zip_range.hpp>
+
+
+#include <string>
+#include <vector>
+#include <boost/foreach.hpp>
+#include <boost/range.hpp>
+#include <pstade/comma_protect.hpp>
+#include <pstade/garlic/back_inserter.hpp>
+#include <pstade/oven/copy.hpp>
+#include <pstade/oven/equal.hpp>
+
+
+void test()
+{
+    using namespace pstade;
+    using namespace oven;
+
+    {
+        std::string src0("0123456");
+        std::string ans0("0123556");
+
+        std::vector<int> src1; {
+            int tmp[] = { 0,1,2,3,4,5,6 };
+            oven::copy(tmp, garlic::back_inserter(src1));
+        }
+        std::vector<int> ans1; {
+            int tmp[] = { 0,1,2,3,5,5,6 };
+            oven::copy(tmp, garlic::back_inserter(ans1));
+        }
+
+        BOOST_FOREACH (
+            comma_protect<void(boost::tuple<char&, int&>)>::type t,
+            oven::make_zip_range(boost::make_tuple(boost::ref(src0), boost::ref(src1)))
+        ) {
+            char& ch = boost::get<0>(t);
+            if (ch == '4')
+                ch = '5';
+
+            int& i = boost::get<1>(t);
+            if (i == 4)
+                i = 5;
+        }
+    
+        BOOST_CHECK(( oven::equals(src0, ans0) ));
+        BOOST_CHECK(( oven::equals(src1, ans1) ));
+    }
+}
+
+
+int test_main(int, char*[])
+{
+    ::test();
+    return 0;
+}
