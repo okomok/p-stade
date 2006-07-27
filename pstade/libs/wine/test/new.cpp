14c187bffbc4adbb8611cc0af8459c55e1da363e
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2006-07-27 12:46:45 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@208 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/libs/wine/test/new.cpp
+++ pstade/libs/wine/test/new.cpp
@@ -29,29 +29,64 @@
 
 
 struct A0 { A0() { } };
-struct A1 { A1(int) { } };
-struct A2 { A2(int, int) { } };
-struct A3 { A3(int, int, int) { } };
-struct A4 { A4(int, int, int, int) { } };
-struct A5 { A5(int, int, int, int, int) { } };
+struct A1 { explicit A1(int) { } };
+struct A2 { A2(int, int&) { } };
+struct A3 { A3(int&, int, int&) { } };
+struct A4 { A4(int, int&, int, int) { } };
+struct A5 { A5(int, int&, int&, int&, int) { } };
+
+
+struct S { S(char const*, int, char const*) { } };
+
+template< class T >
+void foo(std::auto_ptr<T>, std::auto_ptr<T>)
+{ }
 
 
 void test()
 {
+    int i = 0;
+    int const c = 0;
+
     {
-        int i0, i1, i2, i3, i4;
         std::auto_ptr<A0> p0(pstade::new_<A0>());
-        std::auto_ptr<A1> p1(pstade::new_<A1>( 0));
-        std::auto_ptr<A2> p2(pstade::new_<A2>( 0,i1));
-        std::auto_ptr<A3> p3(pstade::new_<A3>(i0, 1,i2)); 
-        std::auto_ptr<A4> p4(pstade::new_<A4>( 0,i1, 2, 3));
-        std::auto_ptr<A5> p5(pstade::new_<A5>( 0, 1, 2,i3,i4));
     }
 
     {
-        std::auto_ptr<A2>     ap(pstade::new_<A2>(0, 1));
-        boost::scoped_ptr<A3> cp(pstade::new_<A3>(0, 1, 2));
-        // boost::shared_ptr<A4> sp(pstade::new_<A4>(0, 1, 2, 3)); // Boost v1.34 -
+        std::auto_ptr<A1> p1(pstade::new_<A1>(i));
+        std::auto_ptr<A1> p2(pstade::new_<A1>(c));
+    }
+    {
+        std::auto_ptr<A2> p1(pstade::new_<A2>(i,i));
+        std::auto_ptr<A2> p2(pstade::new_<A2>(i,i));
+        std::auto_ptr<A2> p3(pstade::new_<A2>(c,i));
+    }
+    {
+        std::auto_ptr<A3> p2(pstade::new_<A3>(i,c,i));
+        std::auto_ptr<A3> p1(pstade::new_<A3>(i,i,i));
+    }
+    {
+        std::auto_ptr<A4> p1(pstade::new_<A4>(c,i,c,c));
+        std::auto_ptr<A4> p2(pstade::new_<A4>(c,i,i,c));
+        std::auto_ptr<A4> p3(pstade::new_<A4>(c,i,c,i));
+    }
+    {
+        std::auto_ptr<A5> p1(pstade::new_<A5>(c,i,i,i,c));
+        std::auto_ptr<A5> p2(pstade::new_<A5>(i,i,i,i,c));
+        std::auto_ptr<A5> p3(pstade::new_<A5>(c,i,i,i,i));
+        std::auto_ptr<A5> p4(pstade::new_<A5>(i,i,i,i,i));
+    }
+    {
+        std::auto_ptr<A2> ap(pstade::new_<A2>(i,i));
+        boost::scoped_ptr<A3> cp(pstade::new_<A3>(i,i,i));
+        // boost::shared_ptr<A4> sp(pstade::new_<A4>(i,i,i,i)); // Boost v1.34 -
+    }
+
+    {
+        ::foo(
+            pstade::new_<S>("exception", i, "safe!"),
+            pstade::new_<S>("exception", i, "safe!")
+        );
     }
 }
 
--- pstade/pstade/new.hpp
+++ pstade/pstade/new.hpp
@@ -16,6 +16,7 @@
 // http://d.hatena.ne.jp/Cryolite/20060622
 // http://www.boost.org/libs/smart_ptr/shared_ptr.htm#BestPractices
 // http://thread.gmane.org/gmane.comp.lib.boost.devel/140159/
+// http://archives.free.net.ph/message/20060328.234524.bf2bc428.en.html
 // http://www.gotw.ca/gotw/056.htm
 
 
@@ -27,13 +28,19 @@
 
 
 #include <memory> // auto_ptr
+#include <boost/config.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/or.hpp>
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/iteration/iterate.hpp>
 #include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/seq/for_each_i.hpp>
 #include <boost/preprocessor/seq/for_each_product.hpp>
+#include <boost/type_traits/is_const.hpp>
+#include <boost/utility/enable_if.hpp> // disable_if
 
 
 #if !defined(PSTADE_NEW_MAX_ARITY)
@@ -41,54 +48,91 @@
 #endif
 
 
+// Workaround:
+// GCC cannot order overloads which needs explicit template parameter
+// if the function argument is const-qualified. 
+// Not that VC++7.1's 'is_const' to array was fixed with Boost v1.34,
+// but it looks still broken under weird situation around 'disable_if'.
+//
+#if defined(__GNUC__)
+    #define PSTADE_NEW_NO_PARTIAL_ORDERING_WITH_EXPLICIT_TEMPLATE_PARAMETER
+#endif
+
+
 namespace pstade {
 
 
-// 0ary
-//
-template< class T > inline
-std::auto_ptr<T>
-new_()
-{
-    return std::auto_ptr<T>( new T() );
-} 
+    // 0ary
+    //
+    template< class T > inline
+    std::auto_ptr<T>
+    new_()
+    {
+        return std::auto_ptr<T>( new T() );
+    } 
 
 
-// 1ary
-//
-template< class T, class A0 > inline
-std::auto_ptr<T>
-new_(A0& a0)
-{
-    return std::auto_ptr<T>( new T(a0) );
-} 
+    // 1ary
+    //
+    template< class T, class A0 > inline
+#if !defined(PSTADE_NEW_NO_PARTIAL_ORDERING_WITH_EXPLICIT_TEMPLATE_PARAMETER)
+    std::auto_ptr<T>
+#else
+    typename boost::disable_if<
+        boost::mpl::or_< boost::is_const<A0> >, // avoid ambiguity
+        std::auto_ptr<T>
+    >::type
+#endif
+    new_(A0& a0)
+    {
+        return std::auto_ptr<T>( new T(a0) );
+    } 
 
-template< class T, class A0 > inline
-std::auto_ptr<T>
-new_(A0 const& a0)
-{
-    return std::auto_ptr<T>( new T(a0) );
-} 
+    template< class T, class A0 > inline
+#if !defined(PSTADE_NEW_NO_PARTIAL_ORDERING_WITH_EXPLICIT_TEMPLATE_PARAMETER)
+    std::auto_ptr<T>
+#else
+    typename boost::disable_if<
+        boost::mpl::or_< boost::is_const<A0> >, // always "false", don't care.
+        std::auto_ptr<T>
+    >::type
+#endif
+    new_(A0 const& a0)
+    {
+        return std::auto_ptr<T>( new T(a0) );
+    }
 
 
-// 2ary -
-//
-#define PSTADE_NEW_call_operator(R, BitSeq) \
+    // 2ary -
+    //
+#define PSTADE_NEW_new(R, BitSeq) \
     template< class T, BOOST_PP_ENUM_PARAMS(n, class A) > inline \
-    std::auto_ptr<T> \
+    PSTADE_NEW_result() \
     new_( BOOST_PP_SEQ_FOR_EACH_I_R(R, PSTADE_NEW_param, ~, BitSeq) ) \
     { \
         return std::auto_ptr<T>( new T( BOOST_PP_ENUM_PARAMS(n, a) ) ); \
     } \
 /**/
 
-#define PSTADE_NEW_arg_type(R, _, Index, Bit) \
-    BOOST_PP_COMMA_IF(Index) BOOST_PP_CAT(PSTADE_NEW_ac, Bit)(BOOST_PP_CAT(A, Index)) \
+#if !defined(PSTADE_NEW_NO_PARTIAL_ORDERING_WITH_EXPLICIT_TEMPLATE_PARAMETER)
+    #define PSTADE_NEW_result() \
+        std::auto_ptr<T> \
+    /**/
+#else
+    #define PSTADE_NEW_result() \
+        typename boost::disable_if< \
+            boost::mpl::or_< BOOST_PP_ENUM(n, PSTADE_NEW_or_arg, ~) >, \
+            std::auto_ptr<T> \
+        >::type \
+    /**/
+#endif
+
+#define PSTADE_NEW_or_arg(Z, Index, _) \
+    boost::is_const< BOOST_PP_CAT(A, Index) > \
 /**/
 
 #define PSTADE_NEW_param(R, _, Index, Bit) \
-    BOOST_PP_COMMA_IF(Index) \
-    BOOST_PP_CAT(A, Index) BOOST_PP_CAT(PSTADE_NEW_c, Bit) & BOOST_PP_CAT(a, Index) \
+    BOOST_PP_COMMA_IF(Index) BOOST_PP_CAT(A, Index) BOOST_PP_CAT(PSTADE_NEW_c, Bit) & BOOST_PP_CAT(a, Index) \
 /**/
 
 #define PSTADE_NEW_c0
@@ -105,8 +149,9 @@ new_(A0 const& a0)
 #undef PSTADE_NEW_c1
 #undef PSTADE_NEW_c0
 #undef PSTADE_NEW_param
-#undef PSTADE_NEW_arg_type
-#undef PSTADE_NEW_call_operator
+#undef PSTADE_NEW_or_arg
+#undef PSTADE_NEW_result
+#undef PSTADE_NEW_new
 
 
 } // namespace pstade
@@ -118,7 +163,7 @@ new_(A0 const& a0)
 
 
 BOOST_PP_SEQ_FOR_EACH_PRODUCT(
-    PSTADE_NEW_call_operator,
+    PSTADE_NEW_new,
     BOOST_PP_REPEAT(n, PSTADE_NEW_bits, ~)
 )
 
