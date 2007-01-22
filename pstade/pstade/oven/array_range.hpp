21b0c8f6099ef79881bdd99d47b91e7bc5913441
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-01-22 09:28:40 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@606 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/oven/array_range.hpp
@@ -0,0 +1,63 @@
+#ifndef PSTADE_OVEN_ARRAY_RANGE_HPP
+#define PSTADE_OVEN_ARRAY_RANGE_HPP
+
+
+// PStade.Oven
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <cstddef> // size_t
+#include <boost/noncopyable.hpp>
+#include <boost/scoped_array.hpp>
+#include <boost/utility/base_from_member.hpp>
+#include "./iter_range.hpp"
+#include "./range_constantable.hpp"
+
+
+namespace pstade { namespace oven {
+
+
+namespace array_range_detail {
+
+
+    template< class Value >
+    struct init
+    {
+        typedef boost::base_from_member<
+            boost::scoped_array<Value>
+        > type;
+    };
+
+
+} // namespace array_range_detail
+
+
+template< class Value >
+struct array_range :
+    private array_range_detail::init<Value>::type,
+    iter_range<Value *>::type,
+    private range_constantable<array_range<Value>, Value const *>,
+    private boost::noncopyable
+{
+    typedef Value const *const_iterator; // "override" it!
+
+private:
+    typedef typename array_range_detail::init<Value>::type init_t;
+    typedef typename iter_range<Value *>::type super_t;
+
+public:
+    explicit array_range(std::size_t sz) :
+        init_t(new Value[sz]),
+        super_t(init_t::member.get(), init_t::member.get() + sz)
+    { }
+};
+
+
+} } // namespace pstade::oven
+
+
+#endif
---
+++ pstade/pstade/oven/copy_range.hpp
@@ -0,0 +1,59 @@
+#ifndef PSTADE_OVEN_COPY_RANGE_HPP
+#define PSTADE_OVEN_COPY_RANGE_HPP
+
+
+// PStade.Oven
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/config.hpp> // BOOST_NESTED_TEMPLATE
+#include <boost/utility/result_of.hpp>
+#include <pstade/adl_barrier.hpp>
+#include <pstade/automatic.hpp>
+#include <pstade/deduced_const.hpp>
+#include <pstade/pipable.hpp>
+#include "./concepts.hpp"
+#include "./extension.hpp"
+
+
+namespace pstade { namespace oven {
+
+
+template< class CopyableRange_ >
+struct op_copy_range
+{
+    typedef CopyableRange_ result_type;
+
+    template< class Range >
+    result_type operator()(Range const& from) const
+    {
+        PSTADE_CONCEPT_ASSERT((SinglePass<Range>));
+        return pstade_oven_extension::Range<CopyableRange_>().
+            BOOST_NESTED_TEMPLATE copy<CopyableRange_>(from);
+    }
+};
+
+
+PSTADE_ADL_BARRIER(copy_range) { // for Boost
+
+template< class CopyableRange_, class Range > inline
+typename boost::result_of<op_copy_range<CopyableRange_>(PSTADE_DEDUCED_CONST(Range)&)>::type
+copy_range(Range const& from)
+{
+    return op_copy_range<CopyableRange_>()(from);
+}
+
+} // ADL barrier
+
+
+PSTADE_PIPABLE(copied, (automatic< op_copy_range<boost::mpl::_1> >))
+
+
+} } // namespace pstade::oven
+
+
+#endif
