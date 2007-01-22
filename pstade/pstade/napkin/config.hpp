4d23dd3ee6070464b9104233fd343ba9bdea68b8
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-01-22 08:59:39 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@599 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/napkin/config.hpp
@@ -0,0 +1,25 @@
+#ifndef PSTADE_NAPKIN_CONFIG_HPP
+#define PSTADE_NAPKIN_CONFIG_HPP
+
+
+// PStade.Napkin
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/config.hpp>
+
+
+#if defined(BOOST_NO_STRINGSTREAM) || \
+    defined(BOOST_NO_STD_WSTRING) || \
+    defined(BOOST_NO_STD_LOCALE)
+    
+    #define PSTADE_NAPKIN_NO_WIDESTRING
+
+#endif
+
+
+#endif
---
+++ pstade/pstade/napkin/ostream.hpp
@@ -0,0 +1,71 @@
+#ifndef PSTADE_NAPKIN_OSTREAM_HPP
+#define PSTADE_NAPKIN_OSTREAM_HPP
+
+
+// PStade.Napkin
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/shared_ptr.hpp>
+#include "./config.hpp"
+#include "./detail/basic_holder.hpp"
+#include "./detail/basic_placeholder.hpp"
+#include "./nout.hpp"
+
+
+namespace pstade { namespace napkin {
+
+
+template< class CharT >
+struct basic_ostream
+{
+    typedef CharT char_type;
+
+    explicit basic_ostream()
+    {
+        reset(nout);
+    }
+
+    template< class StringOutputable >
+    explicit basic_ostream(StringOutputable& out) :
+        m_pout(new detail::basic_holder<CharT, StringOutputable>(out))
+    { }
+
+    template< class OutputStreamable >
+    basic_ostream& operator<<(const OutputStreamable& x)
+    {
+        m_pout->output(x);
+        return *this;
+    }
+
+    template< class StringOutputable >
+    void reset(StringOutputable& out)
+    {
+        m_pout.reset(new detail::basic_holder<CharT, StringOutputable>(out));
+    }
+
+private:
+    boost::shared_ptr< detail::basic_placeholder<CharT> > m_pout;
+};
+
+
+typedef basic_ostream<char>
+ostream;
+
+
+#if !defined(PSTADE_NAPKIN_NO_WIDESTRING)
+
+typedef basic_ostream<wchar_t>
+wostream;
+
+#endif
+
+
+} } // namespace pstade::napkin
+
+
+#endif
