0f138779035871d813ac673e7163c4a2fb8b8f9c
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-02-23 07:56:22 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@728 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/pstade/is_returnable.hpp
+++ pstade/pstade/is_returnable.hpp
@@ -12,7 +12,8 @@
 
 // What:
 //
-// Detects whether 'To foo() { return from; }' is safe or not.
+// Returns true iif 'To foo() { return from; }' is well-defined.
+// Precondition: 'To' is a reference or CopyConstructible.
 
 
 #include <boost/config.hpp>
@@ -36,29 +37,45 @@ namespace pstade {
 
         template<class From, class To>
         struct is_convertible :
-    #if BOOST_WORKAROUND(BOOST_MSVC, == 1310) // VC7.1
+        #if BOOST_WORKAROUND(BOOST_MSVC, == 1310) // VC7.1
             // See the implementation of 'boost::enable_if_convertible'.
             // Just to be safe; 'is_returnable' is often used in 'enable_if'.
             boost::mpl::or_<
                 boost::is_same<From, To>,
                 boost::is_convertible<From, To>
             >
-    #else
+        #else
             boost::is_convertible<From, To>
-    #endif
+        #endif
         { };
 
 
         template<class X, class Y>
         struct is_base_of :
-    #if BOOST_VERSION < 103400
+        #if BOOST_VERSION < 103400
+            // This is the TR1 behavior.
             boost::mpl::or_<
                 boost::is_same<X, Y>,
                 boost::is_base_of<X, Y>
             >
-    #else
+        #else
             boost::is_base_of<X, Y>
-    #endif
+        #endif
+        { };
+
+
+        template<class From, class To>
+        struct not_return_reference_to_temporary :
+            boost::mpl::or_<
+                boost::mpl::not_< boost::is_reference<To> >,
+                boost::mpl::and_<
+                    boost::is_reference<From>,
+                    is_base_of<
+                        typename remove_cvr<To>::type,
+                        typename remove_cvr<From>::type
+                    >
+                >
+            >
         { };
 
 
@@ -69,16 +86,7 @@ namespace pstade {
     struct is_returnable :
         boost::mpl::and_<
             is_returnable_detail::is_convertible<From, To>,
-            boost::mpl::or_<
-                boost::mpl::not_< boost::is_reference<To> >,
-                boost::mpl::and_<
-                    boost::is_reference<From>,
-                    is_returnable_detail::is_base_of<
-                        typename remove_cvr<To>::type,
-                        typename remove_cvr<From>::type
-                    >
-                >
-            >
+            is_returnable_detail::not_return_reference_to_temporary<From, To>
         >
     { };
 
