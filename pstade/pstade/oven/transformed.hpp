8dacd019ccdbe64ace6329a18bd75209a762dabe
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-04-05 06:40:59 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@864 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/pstade/oven/transformed.hpp
+++ pstade/pstade/oven/transformed.hpp
@@ -10,7 +10,7 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
-// Port of: Boost.RangeEx
+// Modeled after: Boost.RangeEx
 //
 // Copyright 2004 Eric Niebler.
 // Distributed under the Boost Software License, Version 1.0.
@@ -35,23 +35,101 @@
 // orthogonal to functor's 'result_type'.
 
 
+#include <boost/iterator/iterator_adaptor.hpp>
 #include <boost/iterator/iterator_traits.hpp>
-#include <boost/iterator/transform_iterator.hpp>
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
 #include <boost/utility/result_of.hpp>
 #include <pstade/callable.hpp>
+#include <pstade/const_fun.hpp>
 #include <pstade/constant.hpp>
 #include <pstade/pass_by.hpp>
 #include <pstade/pipable.hpp>
+#include <pstade/remove_cvr.hpp>
 #include <pstade/use_default.hpp>
 #include "./concepts.hpp"
-#include "./identities.hpp"
+#include "./detail/pure_traversal.hpp"
 #include "./iter_range.hpp"
 #include "./range_iterator.hpp"
 
 
 namespace pstade { namespace oven {
+
+
+namespace transformed_detail {
+
+
+    // IteratorCategory must be recomputed using 'detail::pure_traversal'.
+    // For example, consider 'rng|zipped|unzipped'.
+    // 'UnaryFun' may resurrect lvalue-ness of the base range,
+    // then a RandomAccess*Input* Iterator can turn into the RandomAccess.
+    // Though 'identities' can do the same thing, this will compile faster.
+
+
+    template< class UnaryFun, class Iterator, class Reference, class Value >
+    struct transform_iterator;
+
+
+    template< class UnaryFun, class Iterator, class Reference, class Value >
+    struct transform_iterator_super
+    {
+        typedef
+            boost::iterator_adaptor<
+                transform_iterator<UnaryFun, Iterator, Reference, Value>,
+                Iterator,
+                Value,
+                typename detail::pure_traversal<Iterator>::type,
+                Reference
+            >
+        type;
+    };
+
+
+    template< class UnaryFun, class Iterator, class Reference, class Value >
+    struct transform_iterator :
+        transform_iterator_super<UnaryFun, Iterator, Reference, Value>::type
+    {
+    private:
+        typedef typename transform_iterator_super<UnaryFun, Iterator, Reference, Value>::type super_t;
+
+    public:
+        transform_iterator()
+        { }
+
+        transform_iterator(Iterator const& it, UnaryFun const& fun) :
+            super_t(it), m_fun(fun)
+        { }
+
+        template< class I, class R, class V >
+        transform_iterator(transform_iterator<UnaryFun, I, R, V> const& other,
+            typename boost::enable_if_convertible<I, Iterator>::type * = 0
+        ) :
+            super_t(other.base()), m_fun(other.function())
+        { }
+
+        UnaryFun const& function() const
+        {
+            return m_fun;
+        }
+
+        // boost compatible
+        UnaryFun const& functor() const
+        {
+            return function();
+        }
+
+    private:
+        UnaryFun m_fun;
+
+    friend class boost::iterator_core_access;
+        typename super_t::reference dereference() const
+        {
+            return m_fun(*this->base());
+        }
+    };
+
+
+} // namespace transformed_detail
 
 
 template<
@@ -64,8 +142,13 @@ struct op_make_transformed :
     template< class Myself, class Range, class UnaryFun >
     struct apply
     {
-        typedef typename range_iterator<Range>::type base_iter_t;
-        typedef typename pass_by_value<UnaryFun>::type fun_t;
+        typedef typename
+            range_iterator<Range>::type
+        base_iter_t;
+
+        typedef typename
+            pass_by_value<UnaryFun>::type
+        fun_t;
 
         typedef typename
             boost::iterator_reference<base_iter_t>::type
@@ -74,28 +157,28 @@ struct op_make_transformed :
         typedef typename
             use_default_eval_to<
                 Reference,
-                boost::result_of<fun_t(base_ref_t)>
+                boost::result_of<PSTADE_CONST_FUN_TPL(fun_t)(base_ref_t)>
             >::type
         ref_t;
 
+        typedef typename
+            use_default_eval_to<
+                Value,
+                remove_cvr<ref_t>
+            >::type
+        val_t;
+
         typedef
-            boost::transform_iterator<
-                fun_t, base_iter_t, ref_t, Value
+            transformed_detail::transform_iterator<
+                fun_t,
+                base_iter_t,
+                ref_t,
+                val_t
             >
         iter_t;
 
         typedef
             iter_range<iter_t> const
-        rng_t;
-
-        // Recompute IteratorCategory using 'identities'.
-        // 'UnaryFun' may resurrect lvalue-ness of the base range,
-        // then a RandomAccess*Input* Iterator turns into the RandomAccess.
-        // e.g. applying 'tuple_at' to a tuple of reference.
-        typedef typename
-            boost::result_of<
-                op_make_identities(rng_t)
-            >::type
         type;
     };
 
@@ -104,12 +187,10 @@ struct op_make_transformed :
     {
         PSTADE_CONCEPT_ASSERT((SinglePass<Range>));
 
-        typedef apply<void, Range, UnaryFun> apply_;
-        return make_identities(
-            typename apply_::rng_t(
-                typename apply_::iter_t(boost::begin(rng), fun),
-                typename apply_::iter_t(boost::end(rng),   fun)
-            )
+        typedef typename Result::iterator iter_t;
+        return Result(
+            iter_t(boost::begin(rng), fun),
+            iter_t(boost::end(rng),   fun)
         );
     }
 };
