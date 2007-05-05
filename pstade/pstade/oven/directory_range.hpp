d1432f3be2aa11f9623c0587c89268b73b77589b
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-05-05 14:30:16 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@977 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/pstade/oven/directory_range.hpp
+++ pstade/pstade/oven/directory_range.hpp
@@ -16,6 +16,7 @@
 // But this is "conceptually" a noncopyable range.
 
 
+#include <boost/filesystem/convenience.hpp> // basic_recursive_directory_iterator
 #include <boost/filesystem/operations.hpp> // basic_directory_iterator
 #include <boost/filesystem/path.hpp>
 #include <pstade/implicitly_defined.hpp>
@@ -30,6 +31,10 @@ template< class Path >
 struct basic_directory_range;
 
 
+template< class Path >
+struct basic_recursive_directory_range;
+
+
 namespace directory_range_detail {
 
 
@@ -39,6 +44,17 @@ namespace directory_range_detail {
         typedef
             iter_range<boost::filesystem::basic_directory_iterator<Path>,
                 lightweight_copyable< basic_directory_range<Path> >
+            >
+        type;
+    };
+
+
+    template< class Path >
+    struct recursive_super
+    {
+        typedef
+            iter_range<boost::filesystem::basic_recursive_directory_iterator<Path>,
+                lightweight_copyable< basic_recursive_directory_range<Path> >
             >
         type;
     };
@@ -72,6 +88,34 @@ public:
 typedef basic_directory_range<boost::filesystem::path> directory_range;
 #if !defined(BOOST_FILESYSTEM_NARROW_ONLY)
 typedef basic_directory_range<boost::filesystem::wpath> wdirectory_range;
+#endif
+
+
+template< class Path >
+struct basic_recursive_directory_range :
+    directory_range_detail::recursive_super<Path>::type
+{
+private:
+    typedef typename directory_range_detail::recursive_super<Path>::type super_t;
+    typedef typename super_t::iterator iter_t;
+
+public:
+    explicit basic_recursive_directory_range(Path const& dir) :
+        super_t(iter_t(dir), iter_t())
+    { }
+
+    template< class ErrorCode >
+    explicit basic_recursive_directory_range(Path const& dir, ErrorCode& ec) :
+        super_t(iter_t(dir, ec), iter_t())
+    { }
+
+    PSTADE_IMPLICITLY_DEFINED_COPY_TO_BASE(basic_recursive_directory_range, super_t)
+};
+
+
+typedef basic_recursive_directory_range<boost::filesystem::path> recursive_directory_range;
+#if !defined(BOOST_FILESYSTEM_NARROW_ONLY)
+typedef basic_recursive_directory_range<boost::filesystem::wpath> wrecursive_directory_range;
 #endif
 
 
