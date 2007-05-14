ccdb26fc1453022d6ded37d4fdac58b04cdc5356
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-05-14 03:34:21 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1003 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/candy/candy.hpp
@@ -0,0 +1,27 @@
+#ifndef PSTADE_CANDY_CANDY_HPP
+#define PSTADE_CANDY_CANDY_HPP
+
+
+// PStade.Candy Flag Library
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "./add.hpp"
+#include "./complement.hpp"
+#include "./count.hpp"
+#include "./empty.hpp"
+#include "./flip.hpp"
+#include "./is_subset_of.hpp"
+#include "./mask.hpp"
+#include "./remove.hpp"
+#include "./reset.hpp"
+#include "./set.hpp"
+#include "./test.hpp"
+#include "./test_any.hpp"
+
+
+#endif
---
+++ pstade/pstade/tomato/tomato.hpp
@@ -0,0 +1,42 @@
+#ifndef PSTADE_TOMATO_TOMATO_HPP
+#define PSTADE_TOMATO_TOMATO_HPP
+
+
+// PStade.Tomato ATL/WTL Library
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "./access.hpp"
+#include "./boolean_cast.hpp"
+#include "./c_str.hpp"
+#include "./co_init.hpp"
+#include "./filesystem.hpp"
+#include "./gdi.hpp"
+#include "./geq_ie4.hpp"
+#include "./get.hpp"
+#include "./idle_handling.hpp"
+#include "./is_wce.hpp"
+#include "./make.hpp"
+#include "./menu.hpp"
+#include "./message_filtering.hpp"
+#include "./message_loop.hpp"
+#include "./module_init.hpp"
+#include "./multibyte_to_widechar.hpp"
+#include "./ole_init.hpp"
+#include "./rebar.hpp"
+#include "./rgb.hpp"
+#include "./scoped_handle.hpp"
+#include "./size_initialize.hpp"
+#include "./succeeded.hpp"
+#include "./toolbar.hpp"
+#include "./tstream.hpp"
+#include "./tstring.hpp"
+#include "./widechar_to_multibyte.hpp"
+#include "./window.hpp"
+
+
+#endif
