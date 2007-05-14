41f3e3dbeab1513da96c71407314672fe483919b
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-05-14 03:34:21 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1003 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/apple/apple.hpp
@@ -0,0 +1,51 @@
+#ifndef PSTADE_APPLE_APPLE_HPP
+#define PSTADE_APPLE_APPLE_HPP
+
+
+// PStade.Apple Declaration Library
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "./basic_ostream_fwd.hpp"
+#include "./basic_string_fwd.hpp"
+#include "./deque_fwd.hpp"
+#include "./hash_map_fwd.hpp"
+#include "./hash_set_fwd.hpp"
+#include "./list_fwd.hpp"
+#include "./map_fwd.hpp"
+#include "./pair_fwd.hpp"
+#include "./set_fwd.hpp"
+#include "./vector_fwd.hpp"
+
+#include "./is_basic_string.hpp"
+#include "./is_deque.hpp"
+#include "./is_hash_map.hpp"
+#include "./is_hash_set.hpp"
+#include "./is_list.hpp"
+#include "./is_map.hpp"
+#include "./is_pair.hpp"
+#include "./is_set.hpp"
+#include "./is_vector.hpp"
+
+#include "./is_associative_container.hpp"
+#include "./is_boost_range.hpp"
+#include "./is_container.hpp"
+#include "./is_sequence.hpp"
+
+#include "./has_equal_range.hpp"
+#include "./has_find.hpp"
+#include "./has_lower_bound.hpp"
+#include "./has_range_constructor.hpp"
+#include "./has_remove.hpp"
+#include "./has_remove_if.hpp"
+#include "./has_reverse.hpp"
+#include "./has_sort.hpp"
+#include "./has_unique.hpp"
+#include "./has_upper_bound.hpp"
+
+
+#endif
---
+++ pstade/napkin/napkin.hpp
@@ -0,0 +1,20 @@
+#ifndef PSTADE_NAPKIN_NAPKIN_HPP
+#define PSTADE_NAPKIN_NAPKIN_HPP
+
+
+// PStade.Napkin Trace Library
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "./config.hpp"
+#include "./extension.hpp"
+#include "./nout.hpp"
+#include "./ostream.hpp"
+#include "./ostream_char.hpp"
+
+
+#endif
