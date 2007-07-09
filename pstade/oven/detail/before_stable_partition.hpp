ad58b704a296b2959fbb0390545c615af8353f9b
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-07-09 09:19:02 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1084 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/oven/detail/before_stable_partition.hpp
+++ pstade/oven/detail/before_stable_partition.hpp
@@ -18,8 +18,6 @@
 // See 'stable_partition_adaptive' and 'Temporary_buffer::size()'.
 
 
-
-
 #include <algorithm>
 #if !defined(_ALGORITHM_) // except for VC++ STL
 
@@ -32,7 +30,7 @@
     namespace pstade { namespace oven {
 
         PSTADE_POD_CONSTANT((egg::result_of_pipable<tp_make_identities<std::ptrdiff_t>::type>::type), before_stable_partition)
-            = PSTADE_EGG_PIPABLE_RESULT_INITIALIZATION(BOOST_PP_IDENTITY({{}}));
+            = PSTADE_EGG_PIPABLE_RESULT_INITIALIZER(BOOST_PP_IDENTITY({{}}));
 
     } }
 
