2f0c095b1198b607f72d1f0820a1b7ba60276c4f
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-07-11 13:11:54 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1088 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/libs/oven/test/foreach.cpp
+++ pstade/libs/oven/test/foreach.cpp
@@ -9,35 +9,69 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <pstade/oven/detail/foreach.hpp>
+#include <pstade/oven/foreach.hpp>
 #include <pstade/minimal_test.hpp>
 
 
 #include <string>
 
 
-template<class Range>
-void test_dependent(Range& rng)
-{
-    PSTADE_OVEN_FOREACH_TPL (reference r, rng)
-        r = '2';
+std::string g_singleton("01234");
 
-    PSTADE_OVEN_FOREACH_TPL (value_type v, rng) {
-        BOOST_CHECK(v == '2');
-    }
+std::string& make_string_once()
+{
+    static int count = 0;
+    BOOST_ASSERT(count == 0);
+    ++count;
+    return g_singleton;
 }
 
+std::string& make_const_string_once()
+{
+    static int count = 0;
+    BOOST_ASSERT(count == 0);
+    ++count;
+    return g_singleton;
+}
 
-void pstade_minimal_test()
+template<class Range>
+Range& make_string_once_tpl()
+{
+    static int count = 0;
+    BOOST_ASSERT(count == 0);
+    ++count;
+    return g_singleton;
+}
+
+template<class Range>
+Range const& make_const_string_once_tpl()
+{
+    static int count = 0;
+    BOOST_ASSERT(count == 0);
+    ++count;
+    return g_singleton;
+}
+
+#define PSTADE_contents_check() \
+    BOOST_CHECK(i == 0 ? v == '0' : true); \
+    BOOST_CHECK(i == 1 ? v == '1' : true); \
+    BOOST_CHECK(i == 2 ? v == '2' : true); \
+    BOOST_CHECK(i == 3 ? v == '3' : true); \
+    BOOST_CHECK(i == 4 ? v == '4' : true); \
+    ++i; \
+/**/
+
+
+void test_()
 {
     {
         std::string str;
-        PSTADE_OVEN_FOREACH (value_type v, str)
-            BOOST_CHECK(false);
+        PSTADE_OVEN_FOREACH (v, str)
+            (void)v, BOOST_CHECK(false);
     }
     {
         std::string str("a");
-        PSTADE_OVEN_FOREACH (reference r, str) {
+        PSTADE_OVEN_FOREACH (r, str) {
             r = 'b';
         }
 
@@ -46,31 +80,97 @@ void pstade_minimal_test()
     {
         std::string str("01234");
         int i = 0;
-        PSTADE_OVEN_FOREACH (value_type r, str) {
-            BOOST_CHECK(i == 0 ? r == '0' : true);
-            BOOST_CHECK(i == 1 ? r == '1' : true);
-            BOOST_CHECK(i == 2 ? r == '2' : true);
-            BOOST_CHECK(i == 3 ? r == '3' : true);
-            BOOST_CHECK(i == 4 ? r == '4' : true);
-            ++i;
+        PSTADE_OVEN_FOREACH (v, str) {
+            PSTADE_contents_check()
         }
         BOOST_CHECK(i == 5);
     }
     {
-        std::string str("01234");
         int i = 0;
-        PSTADE_OVEN_FOREACH (reference r, str) {
-            BOOST_CHECK(i == 0 ? r == '0' : true);
-            BOOST_CHECK(i == 1 ? r == '1' : true);
-            BOOST_CHECK(i == 2 ? r == '2' : true);
-            BOOST_CHECK(i == 3 ? r == '3' : true);
-            BOOST_CHECK(i == 4 ? r == '4' : true);
-            ++i;
+        PSTADE_OVEN_FOREACH (v, ::make_string_once()) {
+            PSTADE_contents_check()
+        }
+    }
+    {
+        int i = 0;
+        PSTADE_OVEN_FOREACH (v, ::make_const_string_once()) {
+            PSTADE_contents_check()
+        }
+    }
+}
+
+
+template<class Range>
+void test_tpl()
+{
+    {
+        Range str;
+        PSTADE_OVEN_FOREACH_TPL (v, str)
+            (void)v, BOOST_CHECK(false);
+    }
+    {
+        Range str("a");
+        PSTADE_OVEN_FOREACH_TPL (r, str) {
+            r = 'b';
+        }
+
+        BOOST_CHECK( str == "b" );
+    }
+    {
+        Range str("01234");
+        int i = 0;
+        PSTADE_OVEN_FOREACH_TPL (v, str) {
+            PSTADE_contents_check()
         }
         BOOST_CHECK(i == 5);
     }
+
     {
-        std::string str("abcdefg");
-        ::test_dependent(str);
+        int i = 0;
+        PSTADE_OVEN_FOREACH_TPL (v, ::make_string_once_tpl<Range>()) {
+            PSTADE_contents_check()
+        }
     }
+    {
+        int i = 0;
+        PSTADE_OVEN_FOREACH_TPL (v, ::make_const_string_once_tpl<Range>()) {
+            PSTADE_contents_check()
+        }
+    }
+}
+
+
+#if defined(PSTADE_OVEN_IN_BOOST)
+
+    void test_boost()
+    {
+        {
+            std::string str;
+            BOOST_OVEN_FOREACH (v, str)
+                (void)v, BOOST_CHECK(false);
+        }
+    }
+
+    template<class Range>
+    void test_boost()
+    {
+        {
+            Range str;
+            BOOST_OVEN_FOREACH_TPL (v, str)
+                (void)v, BOOST_CHECK(false);
+        }
+    }
+
+#endif
+
+
+void pstade_minimal_test()
+{
+    ::test_();
+    ::test_tpl<std::string>();
+
+#if defined(PSTADE_OVEN_IN_BOOST)
+    ::test_boost();
+    ::test_boost_tpl<std::string>();
+#endif
 }
