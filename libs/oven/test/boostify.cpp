18b0598b06cdad79155e7e687e93f123ff429855
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-07-14 07:33:47 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1090 350e9bb6-6311-0410-90c3-be67731b76ec

--- libs/oven/test/boostify.cpp
+++ libs/oven/test/boostify.cpp
@@ -9,22 +9,97 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
-#if 1
-    namespace pstade {
-        namespace oven { }
-    }
-
-    namespace boost {
-        namespace oven = pstade::oven;
-    }
-#endif
-
-
+#include <pstade/minimal_test.hpp>
 #include <pstade/oven/oven.hpp>
 
 
-int main()
+#include <boost/range.hpp>
+#include <string>
+
+
+#if !defined(boost_oven_extension)
+    #error extension is not boostified.
+#endif
+
+#if !defined(BOOST_OVEN_EXTENSION_OF_TYPE)
+    #error extension is not boostified.
+#endif
+
+#if !defined(BOOST_OVEN_EXTENSION_OF_TEMPLATE)
+    #error extension is not boostified.
+#endif
+
+
+#if !defined(BOOST_OVEN_FOREACH)
+    #error foreach is not boostified.
+#endif
+
+#if !defined(BOOST_OVEN_FOREACH_TPL)
+    #error foreach is not boostified.
+#endif
+
+
+#if !defined(BOOST_OVEN_CHECKED)
+    #error checked is not boostified.
+#endif
+
+
+namespace Foo {
+
+    template< class T >
+    struct Pair
+    {
+        T first, last;
+    };
+
+} // namespace Foo
+
+
+namespace boost_oven_extension {
+
+    template< class T >
+    struct Range< Foo::Pair<T> >
+    {
+        template< class X >
+        struct associate
+        {
+            typedef T mutable_iterator;
+            typedef T constant_iterator;
+        };
+
+        template< class Iterator, class X >
+        Iterator begin(X& x)
+        {
+            return x.first;
+        }
+
+        template< class Iterator, class X >
+        Iterator end(X& x)
+        {
+            return x.last;
+        }
+    };
+
+} // namespace boost_oven_extension
+
+BOOST_OVEN_EXTENSION_OF_TEMPLATE((Foo)(Pair), (class))
+
+
+void pstade_minimal_test()
 {
-    boost::oven::always("hello, boost");
-    return 0;
+    {
+        namespace oven = boost::oven;
+        std::string str("hello, boost");
+        ::Foo::Pair<std::string::iterator> rng = { str.begin(), str.end() };
+        BOOST_OVEN_FOREACH (ch, rng|oven::identities) {
+            (void)ch;
+        }
+    }
+    {
+        using namespace boost::oven;
+        BOOST_CHECK( equals(std::string("ABC"), std::string("abc")|upper_chars) );
+    }
+    {
+        BOOST_OVEN_CHECKED(std::string("abc"));
+    }
 }
