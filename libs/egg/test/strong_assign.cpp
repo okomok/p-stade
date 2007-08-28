2bb44d56c7dccdaffe6570f5ff2c7c9ba95b5661
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-08-28 00:48:52 +0000

    renamed from swap_assign
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1141 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ libs/egg/test/strong_assign.cpp
@@ -0,0 +1,28 @@
+#include <pstade/vodka/drink.hpp>
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <pstade/egg/strong_assign.hpp>
+#include <pstade/minimal_test.hpp>
+
+
+namespace egg = pstade::egg;
+using namespace egg;
+
+
+void pstade_minimal_test()
+{
+    {
+        int i = 0;
+        BOOST_CHECK( strong_assign(i, 3) == 3 );
+        BOOST_CHECK( (i|strong_assign(4)) == 4 );
+        BOOST_CHECK( i == 4 );
+    }
+}
