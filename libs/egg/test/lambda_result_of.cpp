b741ff20705199160c7a4ddfd2555a33094709bf
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-09-05 23:11:51 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1151 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ libs/egg/test/lambda_result_of.cpp
@@ -0,0 +1,73 @@
+#include <pstade/vodka/drink.hpp>
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <pstade/egg/lambda/result_of.hpp>
+#include <pstade/minimal_test.hpp>
+
+
+#include <pstade/egg/lambda/placeholders.hpp>
+#include <pstade/egg/lambda/bind.hpp>
+#include <pstade/egg/plus.hpp>
+#include <pstade/test.hpp>
+
+
+namespace bll = boost::lambda;
+using namespace pstade::egg;
+using pstade::result_of;
+
+
+PSTADE_TEST_IS_RESULT_OF((int&), op_lambda_1(int&))
+PSTADE_TEST_IS_RESULT_OF((int const&), op_lambda_1(int const&))
+#if defined(PSTADE_EGG_LAMBDA_CAN_TAKE_RVALUES)
+    PSTADE_TEST_IS_RESULT_OF((int const&), op_lambda_1(int))
+    PSTADE_TEST_IS_RESULT_OF((int const&), op_lambda_1(int const))
+#endif
+
+
+void pstade_minimal_test()
+{
+    {
+        int i = 10;
+        result_of<op_lambda_1(int&)>::type r = lambda_1(i);
+        BOOST_CHECK(&r == &i);
+        BOOST_CHECK(r == 10);
+    }
+    {
+        int const i = 10;
+        result_of<op_lambda_1(int const&)>::type r = lambda_1(i);
+        BOOST_CHECK(&r == &i);
+        BOOST_CHECK(r == 10);
+    }
+#if defined(PSTADE_EGG_LAMBDA_CAN_TAKE_RVALUES)
+    {
+        result_of<op_lambda_1(int)>::type r = lambda_1(10);
+        BOOST_CHECK(r == 10);
+    }
+#endif
+    {
+        typedef result_of<op_lambda_bind(op_plus const&, int, op_lambda_1 const&)>::type fun_t;
+        fun_t fun = lambda_bind(plus, 3, lambda_1);
+
+        PSTADE_TEST_IS_RESULT_OF((int), fun_t(int&))
+        PSTADE_TEST_IS_RESULT_OF((int), fun_t(int const&))
+
+#if defined(PSTADE_EGG_LAMBDA_CAN_TAKE_RVALUES)
+        PSTADE_TEST_IS_RESULT_OF((int), fun_t(int))
+        PSTADE_TEST_IS_RESULT_OF((int), fun_t(int const))
+#endif
+
+        int i = 9;
+        BOOST_CHECK( fun(i) == 3+9 );
+#if defined(PSTADE_EGG_LAMBDA_CAN_TAKE_RVALUES)
+        BOOST_CHECK( fun(9) == 3+9 );
+#endif
+    }
+}
