ebc4ab044fbc4cce9779e917da7a3c4d9b1b834e
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-09-23 12:13:29 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1199 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/oven/any_output_iterator.hpp
+++ pstade/oven/any_output_iterator.hpp
@@ -14,14 +14,11 @@
 #include <iterator>
 #include <typeinfo>
 #include <boost/assert.hpp>
-#include <boost/mpl/bool.hpp>
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
 #include <pstade/egg/do_swap.hpp>
 #include <pstade/egg/static_downcast.hpp>
-#include <pstade/enable_if.hpp>
 #include <pstade/make_bool.hpp>
-#include <pstade/remove_cvr.hpp>
 #include <pstade/reset_assignment.hpp>
 #include "./any_iterator_fwd.hpp"
 #include "./write.hpp"
@@ -79,92 +76,6 @@ namespace any_output_iterator_detail {
     };
 
 
-    template< class What >
-    struct content_of
-    {
-        typedef boost::shared_ptr< placeholder<What> > type;
-    };
-
-
-    template< class What >
-    struct output_proxy
-    {
-    private:
-        typedef typename content_of<What>::type content_t;
-
-    public:
-        explicit output_proxy(content_t const& c) :
-            m_content(c)
-        { }
-
-        output_proxy& operator=(What w)
-        {
-            m_content->write_(w);
-            return *this;
-        }
-    private:
-        content_t m_content;
-    };
-
-
-    // See writable_postfix_increment_proxy in Boost.Iterator.
-    template< class What >
-    struct postfix_increment_proxy
-    {
-    private:
-        typedef postfix_increment_proxy self_t;
-        typedef any_output_iterator<What> iter_t;
-        typedef typename content_of<What>::type content_t;
-
-    public:
-        postfix_increment_proxy(iter_t& it, content_t& c, bool& written) :
-            m_it(it), m_content(c), m_written(written)
-        { }
-
-        self_t& operator *()
-        {
-            return *this;
-        }
-
-        // Provides writability of *r++
-        What operator=(What w)
-        {
-            m_content->write_(w);
-            m_content->increment();
-            m_written = true;
-            return w;
-        }
-
-        // Provides X(r++)
-        operator iter_t const&() const
-        {
-            return m_it;
-        }
-
-        // Provides r++
-        ~postfix_increment_proxy()
-        {
-            if (!m_written)
-                m_content->increment();
-        }
-
-    private:
-        iter_t& m_it;
-        content_t& m_content;
-        bool& m_written;
-    };
-
-    template<class X>
-    struct is_postfix_increment_proxy :
-        boost::mpl::false_
-    { };
-
-    template<class What>
-    struct is_postfix_increment_proxy< postfix_increment_proxy<What> > :
-        boost::mpl::true_
-    { };
-
-
 } // namespace any_output_iterator_detail
 
 
@@ -174,9 +85,7 @@ struct any_output_iterator :
 {
 private:
     typedef any_output_iterator self_t;
-    typedef any_output_iterator_detail::output_proxy<What> output_proxy_t;
-    typedef any_output_iterator_detail::postfix_increment_proxy<What> postfix_increment_proxy_t;
-    typedef typename any_output_iterator_detail::content_of<What>::type content_t;
+    typedef boost::shared_ptr< any_output_iterator_detail::placeholder<What> > content_t;
 
     template< class Iterator >
     struct holder_of
@@ -190,9 +99,7 @@ public:
     { }
 
     template< class Iterator >
-    explicit any_output_iterator(Iterator it,
-        typename disable_if< any_output_iterator_detail::is_postfix_increment_proxy<Iterator> >::type = 0
-    ) :
+    explicit any_output_iterator(Iterator it) :
         m_content(new typename holder_of<Iterator>::type(it))
     { }
 
@@ -246,42 +153,31 @@ public:
         return egg::static_downcast<typename holder_of<Iterator>::type>(*m_content).held();
     }
 
-// iterator implementation
-    output_proxy_t operator *() const
-    {
-        BOOST_ASSERT(m_content);
-        return output_proxy_t(m_content);
-    }
-
-    self_t& operator++()
-    {
-        BOOST_ASSERT(m_content);
-        m_content->increment();
-        return *this;
-    }
-
-#if 0
-    // OutputIterator has SinglePass semantics,
-    // but it++ is a MultiPass algorithm. Doh!
-    self_t  operator++(int)
-    {
-        BOOST_ASSERT(m_content);
-        self_t tmp(*this);
-        ++*this;
-        return tmp; // This is no longer a valid iterator.
-    }
-#endif
-
-    postfix_increment_proxy_t operator++(int)
-    {
-        BOOST_ASSERT(m_content);
-        m_postfix_written = false;
-        return postfix_increment_proxy_t(*this, m_content, m_postfix_written);
-    }
-
 private:
     content_t m_content;
-    bool m_postfix_written;
+
+// iterator implementation
+    struct output_proxy
+    {
+        content_t m_content;
+
+        void operator=(What w)
+        {
+            m_content->write_(w);
+            m_content->increment();
+        }
+    };
+
+public:
+    output_proxy operator *() const
+    {
+        BOOST_ASSERT(m_content);
+        output_proxy r = {m_content};
+        return r;
+    }
+
+    self_t& operator++() { return *this; }
+    self_t& operator++(int) { return *this; }
 };
 
 
