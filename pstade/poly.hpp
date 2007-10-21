b7c5b7a40ca4d308d366857bc89d5c54aa2b1c5d
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-10-21 11:02:30 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1256 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/poly.hpp
+++ pstade/poly.hpp
@@ -11,6 +11,13 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
+// Copyright (C) 2007, Corrado Zoccolo
+//
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
 // See:
 //
 // Boost.Function
