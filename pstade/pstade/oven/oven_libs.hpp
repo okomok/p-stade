cf570f55ec58ce9311399994fa47f9259d109e35
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-12-03 08:13:25 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1332 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/oven/oven_libs.hpp
@@ -0,0 +1,20 @@
+#ifndef PSTADE_OVEN_OVEN_LIBS_HPP
+#define PSTADE_OVEN_OVEN_LIBS_HPP
+
+
+// PStade.Oven
+//
+// Copyright Shunsuke Sogame 2005-2007.
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "./block.hpp"
+#include "./directory_range.hpp"
+#include "./matches.hpp"
+#include "./regex.hpp"
+#include "./tokenized.hpp"
+
+
+#endif
