f2468e5ea0312bd35dc30b090806ceeb8f5d2b2b
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-12-06 05:35:22 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/pstade@1345 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/egg/detail/use_brace_leveln.hpp
@@ -0,0 +1,32 @@
+#ifndef PSTADE_EGG_DETAIL_USE_BRACE_LEVELN_HPP
+#define PSTADE_EGG_DETAIL_USE_BRACE_LEVELN_HPP
+#include "./prefix.hpp"
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/mpl/limits/arity.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/type.hpp>
+
+
+#define PSTADE_EGG_REPEAT_BRACE_L(N) BOOST_PP_REPEAT(N, PSTADE_EGG_REPEAT_BRACE_L_op, ~)
+#define PSTADE_EGG_REPEAT_BRACE_L_op(Z, N, _) {
+#define PSTADE_EGG_REPEAT_BRACE_R(N) BOOST_PP_REPEAT(N, PSTADE_EGG_REPEAT_BRACE_R_op, ~)
+#define PSTADE_EGG_REPEAT_BRACE_R_op(Z, N, _) }
+
+
+#define PSTADE_EGG_USE_BRACE_LEVELN() <pstade/egg/detail/use_brace_leveln_iteration.hpp>
+
+
+#endif
