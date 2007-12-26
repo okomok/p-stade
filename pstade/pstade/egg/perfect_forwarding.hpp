cf1f23f4255d4a2968603bfcd6fccdb687452aed
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2007-12-26 23:10:46 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1393 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/egg/perfect_forwarding.hpp
@@ -0,0 +1,36 @@
+#ifndef PSTADE_EGG_PERFECT_FORWARDING_HPP
+#define PSTADE_EGG_PERFECT_FORWARDING_HPP
+#include "./detail/prefix.hpp"
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/preprocessor/arithmetic/dec.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/seq/elem.hpp>
+#include <boost/preprocessor/seq/for_each_i.hpp>
+#include <boost/preprocessor/seq/for_each_product.hpp>
+#include <boost/preprocessor/seq/size.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <pstade/deduced_const.hpp>
+#include <pstade/preprocessor.hpp>
+#include "./detail/call_baby.hpp"
+#include "./detail/meta_arg.hpp"
+#include "./detail/nullary_result.hpp"
+#include "./function_fwd.hpp"
+#include "./sig_template.hpp"
+
+
+#define PSTADE_EGG_PERFECT_FORWARDING() <pstade/egg/detail/perfect_forwarding_include.hpp>
+
+
+#endif
