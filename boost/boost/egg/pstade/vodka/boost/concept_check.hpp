5c1590bda43912335092dba0937e4554cf3040ab
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-01-14 15:40:07 +0000

    cleanup 2007
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1476 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/boost/egg/pstade/vodka/boost/concept_check.hpp
+++
@@ -1,8 +0,0 @@
-
-
-#if defined(_MSC_VER)
-    #pragma warning(push)
-    #pragma warning(disable: 4100) // unreferenced formal parameter
-    #include <boost/concept_check.hpp>
-    #pragma warning(pop)
-#endif
