80665748cf205bfd55bd9ad15b45b1eaae155d67
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-01-23 05:21:32 +0000

    adapted_to documented.
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1660 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/libs/egg/example/adapted_to_comparison.cpp
@@ -0,0 +1,56 @@
+#include <pstade/vodka/drink.hpp>
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <pstade/egg/adapted_to.hpp>
+#include <pstade/egg/curry.hpp>
+#include <pstade/minimal_test.hpp>
+
+
+namespace egg = pstade::egg;
+using namespace egg;
+
+
+//[code_adapted_to_comparison
+struct base_plus
+{
+    int id;
+
+    typedef int result_type;
+
+    result_type operator()(int x, int y) const
+    {
+        return x + y;
+    }
+};
+
+typedef result_of_curry2<base_plus>::type T_curried_plus;
+T_curried_plus const curried_plus777 = PSTADE_EGG_CURRY2({777});
+T_curried_plus const curried_plus999 = PSTADE_EGG_CURRY2({999});
+
+bool operator==(T_curried_plus const &left, T_curried_plus const& right)
+{
+    return egg::adapted_to<base_plus>(left).id == egg::adapted_to<base_plus>(right).id;
+}
+
+void test()
+{
+    BOOST_CHECK( curried_plus777(4)(9) == 4+9 );
+    BOOST_CHECK( egg::adapted_to<base_plus>(curried_plus777).id == 777);
+    BOOST_CHECK( curried_plus777 == curried_plus777 );
+    BOOST_CHECK(!(curried_plus777 == curried_plus999) );
+}
+//]
+
+
+void pstade_minimal_test()
+{
+    test();
+}
