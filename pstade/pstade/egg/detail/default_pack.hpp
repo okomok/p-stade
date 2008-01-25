e1685d959959f802531cf85d5768ca41be4f177e
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-01-25 12:26:57 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1716 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/egg/detail/default_pack.hpp
@@ -0,0 +1,28 @@
+#ifndef PSTADE_EGG_DETAIL_DEFAULT_PACK_HPP
+#define PSTADE_EGG_DETAIL_DEFAULT_PACK_HPP
+#include "./prefix.hpp"
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "../config.hpp" // PSTADE_EGG_HAS_FUSIONS
+
+
+#if defined(PSTADE_EGG_HAS_FUSIONS)
+    #include "../fusion/pack.hpp"
+    #define PSTADE_EGG_DEFAULT_PACK pstade::egg::X_fusion_pack
+    #define PSTADE_EGG_DEFAULT_PACK_INIT PSTADE_EGG_FUSION_PACK_INIT
+#else
+    #include "../tuple/pack.hpp"
+    #define PSTADE_EGG_DEFAULT_PACK pstade::egg::X_tuple_pack
+    #define PSTADE_EGG_DEFAULT_PACK_INIT PSTADE_EGG_TUPLE_PACK_INIT
+#endif
+
+
+#endif
