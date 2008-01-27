71a899daddfe28a94b9aa84b9bf96f66801b5d42
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-01-27 12:45:29 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1746 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/libs/egg/example/unnamed_recursion.cpp
@@ -0,0 +1,86 @@
+#include <pstade/vodka/drink.hpp>
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <pstade/egg/lazy.hpp>
+#include <pstade/egg/fix.hpp>
+#include <pstade/egg/compose.hpp>
+#include <pstade/egg/curry.hpp>
+#include <pstade/egg/bll/placeholders.hpp>
+#include <pstade/egg/bll/result_of.hpp>
+#include <boost/lambda/core.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/if.hpp>
+#include <pstade/minimal_test.hpp>
+#include <pstade/egg/const.hpp>
+#include <boost/function.hpp>
+
+
+namespace bll = boost::lambda;
+namespace egg = pstade::egg;
+using namespace egg;
+
+
+struct T_inc
+{
+    typedef int result_type;
+
+    result_type operator()(int i) const
+    {
+        return i + 1;
+    }
+};
+
+PSTADE_EGG_CONST((T_inc), inc) = {};
+
+
+void unnamed_recursion()
+{
+    T_bll_1 const _1 = bll_1;
+    T_bll_2 const _2 = bll_2;
+
+    int const two = 2, ten = 10;
+
+    BOOST_CHECK(
+        lazy(_1)(_2 - 1)(inc, ten) == inc(10-1)
+    );
+
+    BOOST_CHECK(
+        compose(curry2(_2 * _1), _2)(two, ten)(two) == 10*2
+    );
+
+    BOOST_CHECK(
+        (lazy(_2 * bll::protect(_1))(lazy(_1)(_2 - 1)))(inc, ten) == 10 * inc(10-1)
+    );
+
+    BOOST_CHECK(
+        (_2 * lazy(_1)(_2 - 1))(inc, ten) == 10 * inc(10-1)
+    );
+
+//[code_unnamed_recursion
+int r =
+    fix(curry2(
+        bll::ret<int>(
+            bll::if_then_else_return( _2 == 0,
+                bll::constant(1),
+                _2 * lazy(_1)(_2 - 1)
+            )
+        )
+    )) (5);
+
+BOOST_CHECK(r == 5*4*3*2*1);
+//]
+}
+
+
+void pstade_minimal_test()
+{
+    ::unnamed_recursion();
+}
