8e7497a38a949fc95aefda1b37f343c61dd236e0
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-01-30 16:40:23 +0000

    better introduction
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1800 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/libs/egg/example/introduction.cpp
+++ pstade/libs/egg/example/introduction.cpp
@@ -10,23 +10,65 @@
 
 
 #include <pstade/minimal_test.hpp>
-
-#include <ctype.h> // old header for mingw+gcc weird name lookup.
 #include <pstade/egg/pipable.hpp>
-using namespace pstade::egg;
-//[code_introduction
-// This initialization incurs no runtime overhead.
-result_of_pipable<int(*)(int)>::type const
-    to_upper = PSTADE_EGG_PIPABLE(&toupper);
+#include <pstade/result_of.hpp>
+#include <boost/type_traits/remove_cv.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/core.hpp>
+#include <boost/lambda/lambda.hpp>
 
-void test()
+
+using pstade::result_of;
+using namespace pstade::egg;
+
+
+//[code_introduction
+struct little_plus
 {
-    int maybeI = 'i'|to_upper|pipable(&tolower)|to_upper();
-    BOOST_CHECK(maybeI == 'I');
+    template<class Self, class X, class Y>
+    struct apply :
+        boost::remove_cv<X>
+    { };
+
+    template<class Auto, class X, class Y>
+    Auto call(X& x, Y& y) const
+    {
+        return x + y;
+    }
+};
+
+// Building LittleFunction into MajorFunction.
+typedef function<little_plus> T_plus;
+T_plus const plus = {{}};
+
+void test_builder()
+{
+    // Boost.ResultOf compatible
+    result_of<T_plus(int, int)>::type r = plus(1, 2);
+    BOOST_CHECK(r == 3);
+
+    // Boost.Lambda compatible
+    namespace bll = boost::lambda;
+    int one = 1;
+    BOOST_CHECK(3 == bll::bind(plus, bll::_1, 2)(one));
+}
+
+// Static initialization which incurs no runtime overhead.
+result_of_pipable<T_plus>::type const my_plus = PSTADE_EGG_PIPABLE({{}});
+
+void test_adaptor()
+{
+    int r = 1|my_plus(2);
+    BOOST_CHECK(r == 3);
+
+    // Dynamic initialization without macros.
+    result_of<T_pipable(T_plus)>::type your_plus = pipable(plus);
+    BOOST_CHECK((1|your_plus(2)) == 3);
 }
 //]
 
 void pstade_minimal_test()
 {
-    ::test();
+    ::test_builder();
+    ::test_adaptor();
 }
