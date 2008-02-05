0ee3e0e2ab748d528ac8b0281ac59cc77d8d9c1c
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-05 23:25:48 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1897 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/pstade/egg/detail/boostify.hpp
@@ -0,0 +1,22 @@
+#ifndef PSTADE_EGG_DETAIL_BOOSTIFY_HPP
+#define PSTADE_EGG_DETAIL_BOOSTIFY_HPP
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+namespace pstade {
+    namespace egg { }
+}
+
+namespace boost {
+    namespace egg = pstade::egg;
+}
+
+
+#endif
---
+++ pstade/pstade/egg/detail/suffix.hpp
@@ -0,0 +1,13 @@
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#if defined(BOOST_MSVC)
+    #pragma warning(pop)
+#endif
