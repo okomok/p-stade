c0a4e0bcc71b12a42059a05ac468b671dc3dd48f
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-07 18:36:29 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1913 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ pstade/libs/egg/test/__cv_ignored.cpp
@@ -0,0 +1,24 @@
+#include <pstade/vodka/drink.hpp>
+
+
+// PStade.Egg
+//
+// Copyright Shunsuke Sogame 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <pstade/minimal_test.hpp>
+
+
+template<class T>
+void foo(T const&) {}
+
+void bar() {}
+
+
+void pstade_minimal_test()
+{
+    ::foo(bar);
+}
