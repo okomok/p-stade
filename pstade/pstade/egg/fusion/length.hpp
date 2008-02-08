09f719a6c3ab0018e8a2f4fac49390a536355f55
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-08 08:43:56 +0000

    mpl adaptation installed.
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@1924 350e9bb6-6311-0410-90c3-be67731b76ec

--- pstade/pstade/egg/fusion/length.hpp
+++ pstade/pstade/egg/fusion/length.hpp
@@ -16,13 +16,11 @@
 // It is guaranteed that return type is `mpl::int_`.
 
 
-#include <boost/mpl/eval_if.hpp>
 #include <boost/mpl/int.hpp>
 #include <boost/mpl/size.hpp>
-#include <boost/tuple/tuple.hpp>
 #include <boost/type_traits/remove_cv.hpp>
-#include <pstade/apple/is_boost_tuple.hpp>
-#include <pstade/apple/pair_fwd.hpp>
+#include <pstade/mpl_boost_tuple.hpp>
+#include <pstade/mpl_std_pair.hpp>
 #include "../config.hpp" // PSTADE_EGG_HAS_FUSIONS
 
 #if defined(PSTADE_EGG_HAS_FUSIONS)
@@ -33,41 +31,11 @@
 namespace pstade { namespace egg {
 
 
-    namespace fusion_length_detail {
-
-
-        template<class Tuple>
-        struct tuple_length :
-            boost::mpl::int_<boost::tuples::length<Tuple>::value>
-        { };
-
-
-        template<class Seq>
-        struct mpl_size :
-            boost::mpl::int_<boost::mpl::size<Seq>::type::value>
-        { };
-
-
-        template<class Tuple>
-        struct aux_ :
-            boost::mpl::eval_if< apple::is_boost_tuple<Tuple>,
-                tuple_length<Tuple>,
-                mpl_size<Tuple>
-            >::type
-        { };
-
-        template<class T, class U>
-        struct aux_< std::pair<T, U> > :
-            boost::mpl::int_<2>
-        { };
-
-
-    } // namespace fusion_length_detail
-
-
     template<class Tuple>
     struct fusion_length :
-        fusion_length_detail::aux_<typename boost::remove_cv<Tuple>::type>
+        boost::mpl::int_<
+            boost::mpl::size<typename boost::remove_cv<Tuple>::type>::type::value
+        >
     { };
 
 
