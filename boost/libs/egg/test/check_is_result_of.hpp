d8b521b98f74350eddfdc53b317b964eac73834e
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-18 18:03:50 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2065 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ boost/libs/egg/test/check_is_result_of.hpp
@@ -0,0 +1,27 @@
+#ifndef BOOST_LIBS_EGG_TEST_CHECK_IS_RESULT_OF_HPP
+#define BOOST_LIBS_EGG_TEST_CHECK_IS_RESULT_OF_HPP
+
+
+// Boost.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/mpl/assert.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/egg/result_of.hpp>
+#include <boost/egg/detail/unparen.hpp>
+
+
+#define CHECK_IS_RESULT_OF(Result, FunCall) \
+    BOOST_MPL_ASSERT((boost::is_same< \
+        BOOST_EGG_UNPAREN(Result), \
+        boost::egg::result_of<FunCall>::type \
+    >)); \
+/**/
+
+
+#endif
