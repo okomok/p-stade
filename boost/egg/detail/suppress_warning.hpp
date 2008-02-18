df121f3354bd39b52b2c2ea430cfd3e693de1ff7
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-18 18:08:10 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2067 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/egg/detail/suppress_warning.hpp
+++ boost/egg/detail/suppress_warning.hpp
@@ -2,7 +2,7 @@
 #define BOOST_EGG_DETAIL_SUPPRESS_WARNING_HPP
 
 
-// PStade.Wine
+// Boost.Egg
 //
 // Copyright Shunsuke Sogame 2005-2008.
 // Distributed under the Boost Software License, Version 1.0.
