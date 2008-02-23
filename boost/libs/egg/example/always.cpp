0b057a7aa0e174628925618229448f97a6c25e22
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-23 00:55:14 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2161 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ boost/libs/egg/example/always.cpp
@@ -0,0 +1,26 @@
+
+
+// Boost.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/egg/always.hpp>
+#include <string>
+#include <boost/utility/addressof.hpp>
+
+
+#include "./egg_example.hpp"
+
+
+//[code_example_always
+void egg_example()
+{
+    int ten = 10;
+    BOOST_CHECK( always(ten)(1,2,3,4,5) == 10 );
+    BOOST_CHECK( boost::addressof(always_ref(ten)(1,2,3,4,5)) == boost::addressof(ten) );
+}
+//]
---
+++ boost/libs/egg/example/using_bll.hpp
@@ -0,0 +1,20 @@
+#ifndef BOOST_LIBS_EGG_EXAMPLE_USING_BLL_HPP
+#define BOOST_LIBS_EGG_EXAMPLE_USING_BLL_HPP
+
+
+// Boost.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/lambda/core.hpp>
+#include <boost/lambda/lambda.hpp>
+
+
+namespace bll = boost::lambda;
+
+
+#endif
