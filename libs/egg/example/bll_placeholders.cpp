816d36c188c53677d25f642337c60d8803ad3f46
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-23 22:46:47 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2182 350e9bb6-6311-0410-90c3-be67731b76ec

--- libs/egg/example/bll_placeholders.cpp
+++ libs/egg/example/bll_placeholders.cpp
@@ -9,7 +9,9 @@
 
 
 #include <boost/egg/bll/placeholders.hpp>
+#include <boost/egg/bll/bind.hpp>
 #include <boost/egg/result_of.hpp>
+#include <functional> // plus
 
 
 #include "./egg_example.hpp"
@@ -18,8 +20,13 @@
 //[code_example_bll_placeholders
 void egg_example()
 {
-    int ten = 10;
-    result_of_<T_bll_1(int &)>::type r = bll_1(ten);
-    BOOST_CHECK( r == 10 );
+    std::plus<int> plus;
+
+    result_of_<
+        T_bll_bind(std::plus<int> &, T_bll_1 const &, int)
+    >::type
+        f = bll_bind(plus, bll_1, 10);
+
+    BOOST_CHECK( f(2) == 12 );
 }
 //]
