620d4103ba8adfd3955f8a0b4a041fffe327ab2a
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-25 01:11:44 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2195 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/libs/egg/test/multiple_defs1_noglob.cpp
+++ boost/libs/egg/test/multiple_defs1_noglob.cpp
@@ -12,9 +12,6 @@
 #include "./egg_all.hpp" // check include guards.
 
 
-// #include "./egg_test.hpp"
-
-
 #if defined(_CRT_SECURE_NO_DEPRECATE)
 #error oops.
 #endif
@@ -31,6 +28,51 @@
 
 
 using namespace boost::egg;
+using function;
+using function_facade;
+using generator;
+using implicit;
+using poly;
+using static_;
+using variadic;
+using ambi1;
+using compose;
+using curry2;
+using uncurry;
+using fix;
+using flip;
+using fuse;
+using unfuse;
+using indirect;
+using lazy;
+using memoize;
+using mono;
+using perfect;
+using pipable;
+using regular;
+using return_;
+using tagged;
+using adapted_to;
+using always;
+using identity;
+using apply;
+using bll_bind;
+using bll_1;
+using X_construct;
+using X_construct_braced1;
+using X_construct_braced2;
+using X_construct_variadic1;
+using get;
+using pack;
+using result_of_;
+#if !defined(BOOST_EGG_CONST)
+    #error oops.
+#endif
+using apply_little;
+using call_little;
+#if !defined(BOOST_EGG_DEDUCED_CONST)
+    #error oops.
+#endif
 
 
 // warning check: This must be shown in IDE.
