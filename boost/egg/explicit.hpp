a5fd55c8e0c77ddf07d37f0cd24e835d9063dea5
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-27 07:49:32 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2256 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/egg/explicit.hpp
+++ boost/egg/explicit.hpp
@@ -21,67 +21,42 @@
 #if defined(BOOST_EGG_HAS_RVALUE_REFS)
 
 
-    #include <boost/preprocessor/repetition/enum.hpp>
-    #include <boost/preprocessor/repetition/enum_binary_params.hpp>
-    #include <boost/preprocessor/repetition/enum_trailing_params.hpp>
-    #include <boost/egg/detail/std_forward.hpp>
-    #include <boost/egg/result_of.hpp>
-
-
-    #define BOOST_EGG_EXPLICIT(N, Name, X_, Params) \
-        BOOST_EGG_EXPLICIT_aux(N, Name, X_, BOOST_EGG_PP_TO_TEMPLATE_PARAM_SEQ(Params)) \
-    /**/
-
-        #define BOOST_EGG_EXPLICIT_aux(N, Name, X_, Params) \
-            template<BOOST_EGG_PP_SEQ_ENUM_PARAMS(Params, T) BOOST_PP_ENUM_TRAILING_PARAMS(N, class A)> inline \
-            typename boost::egg::result_of_<X_<BOOST_EGG_PP_SEQ_ENUM_ARGS(Params, T)>( \
-                BOOST_PP_ENUM_PARAMS(N, A) \
-            )>::type \
-            BOOST_EGG_name(BOOST_PP_ENUM_BINARY_PARAMS(N, A, &&a)) \
-            { \
-                return X_<BOOST_EGG_PP_SEQ_ENUM_ARGS(Params, T)>()( \
-                    BOOST_PP_ENUM(N, BOOST_EGG_EXPLICIT_fwd, A) \
-                ); \
-            } \
-        /**/
-
-        #define BOOST_EGG_EXPLICIT_fwd(Z, N, A) boost::egg::std_forward<BOOST_PP_CAT(A, N)>(BOOST_PP_CAT(a, N))
+    // TODO
 
 
 #else
 
 
     #include <boost/preprocessor/array/elem.hpp>
-    #include <boost/preprocessor/facilities/intercept.hpp>
     #include <boost/preprocessor/repetition/enum_params.hpp>
     #include <boost/preprocessor/seq/size.hpp>
+    #include <boost/preprocessor/seq/transform.hpp>
+    #include <boost/egg/detail/bytag_to.hpp>
     #include <boost/egg/detail/const_overloaded.hpp>
     #include <boost/egg/detail/ns_result_of.hpp>
-    #include <boost/egg/preprocessor/bits_enum.hpp>
-    #include <boost/egg/preprocessor/bits_enum_binary_params.hpp>
-    #include <boost/egg/preprocessor/for_each_bits.hpp>
+    #include <boost/egg/preprocessor/seq_enum_i.hpp>
+    #include <boost/egg/preprocessor/seq_for_each_product.hpp>
 
 
-    #define BOOST_EGG_EXPLICIT(N, Name, X_, Params) \
-        BOOST_EGG_PP_FOR_EACH_BITS(N, BOOST_EGG_EXPLICIT_op, (3, (Name, X_, BOOST_EGG_PP_TO_TEMPLATE_PARAM_SEQ(Params)))) \
+    #define BOOST_EGG_EXPLICIT(Name, X_, Params, BytagSeq) \
+        BOOST_EGG_PP_SEQ_FOR_EACH_PRODUCT( \
+            BOOST_PP_SEQ_TRANSFORM(BOOST_EGG_BYTAG_TO_SEQ, ~, BytagSeq), BOOST_EGG_EXPLICIT_op, (3, (Name, X_, BOOST_EGG_PP_TO_TEMPLATE_PARAM_SEQ(Params))) \
+        ) \
     /**/
 
-        #define BOOST_EGG_EXPLICIT_op(R, Bits, Nm_X_P) \
-            template<BOOST_EGG_PP_SEQ_ENUM_PARAMS(BOOST_PP_ARRAY_ELEM(2, Nm_X_P), T), BOOST_PP_ENUM_PARAMS(BOOST_PP_SEQ_SIZE(Bits), class A)> \
+        #define BOOST_EGG_EXPLICIT_op(R, BytagSeq, Nm_X_P) \
+            template<BOOST_EGG_PP_SEQ_ENUM_PARAMS(BOOST_PP_ARRAY_ELEM(2, Nm_X_P), T), BOOST_PP_ENUM_PARAMS(BOOST_PP_SEQ_SIZE(BytagSeq), class A)> \
             BOOST_EGG_NS_RESULT_OF( \
                 (BOOST_PP_ARRAY_ELEM(1, Nm_X_P)<BOOST_EGG_PP_SEQ_ENUM_ARGS(BOOST_PP_ARRAY_ELEM(2, Nm_X_P), T)>), \
-                (BOOST_PP_SEQ_SIZE(Bits), (BOOST_EGG_PP_BITS_ENUM_R(R, Bits, BOOST_EGG_EXPLICIT_op_w0, BOOST_EGG_EXPLICIT_op_w1, ~))) \
+                (BOOST_PP_SEQ_SIZE(BytagSeq), (BOOST_EGG_PP_SEQ_ENUM_I_R(R, BytagSeq, BOOST_EGG_BYTAG_TO_NS_DEDUCED, ~))) \
             ) \
-            BOOST_PP_ARRAY_ELEM(0, Nm_X_P)(BOOST_EGG_PP_BITS_ENUM_BINARY_PARAMS_R(R, Bits, A, &a) BOOST_EGG_CONST_OVERLOADED(BOOST_PP_SEQ_SIZE(Bits), A)) \
+            BOOST_PP_ARRAY_ELEM(0, Nm_X_P)(BOOST_EGG_PP_SEQ_ENUM_I_R(R, BytagSeq, BOOST_EGG_BYTAG_TO_PARAM, ~) BOOST_EGG_CONST_OVERLOADED(BOOST_PP_SEQ_SIZE(BytagSeq), A)) \
             { \
                 return BOOST_PP_ARRAY_ELEM(1, Nm_X_P)<BOOST_EGG_PP_SEQ_ENUM_ARGS(BOOST_PP_ARRAY_ELEM(2, Nm_X_P), T)>()( \
-                    BOOST_PP_ENUM_PARAMS(BOOST_PP_SEQ_SIZE(Bits), a) \
+                    BOOST_PP_ENUM_PARAMS(BOOST_PP_SEQ_SIZE(BytagSeq), a) \
                 ); \
             } \
         /**/
-
-        #define BOOST_EGG_EXPLICIT_op_w0(R, I, _) BOOST_PP_CAT(A, I) &
-        #define BOOST_EGG_EXPLICIT_op_w1(R, I, _) BOOST_EGG_NS_CONST_REF(BOOST_PP_CAT(A, I))
 
 
 #endif // defined(BOOST_EGG_HAS_RVALUE_REFS)
