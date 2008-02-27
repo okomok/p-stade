78d723db54e6381eb615ff05e6567170bf075e3f
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-27 07:14:17 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2255 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ boost/libs/egg/test/pp_seq.cpp
@@ -0,0 +1,40 @@
+
+
+// Boost.Egg
+//
+// Copyright Shunsuke Sogame 2007-2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/egg/preprocessor/seq_enum_i.hpp>
+#include <boost/egg/preprocessor/seq_for_each_product.hpp>
+#include "./egg_test.hpp"
+
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/cat.hpp>
+
+
+void bar(int,  int , int)
+{ }
+
+
+#define MACRO2(R, Data, I, Elem) (Data)Elem
+
+
+#define MACRO(R, Seq, Data) \
+    void BOOST_PP_CAT(foo, BOOST_PP_SEQ_CAT(Seq))() \
+    { \
+        Data i = BOOST_PP_SEQ_CAT(Seq); \
+        ::bar( BOOST_EGG_PP_SEQ_ENUM_I_R(R, Seq, MACRO2, int) ); \
+    } \
+/**/
+
+BOOST_EGG_PP_SEQ_FOR_EACH_PRODUCT( ((1)(2)) ((0)) ((3)(4)) , MACRO, long)
+
+
+void egg_test()
+{
+}
