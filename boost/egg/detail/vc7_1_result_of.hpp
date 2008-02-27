7748c233c17c86e15d38f245c9797c1dbc936689
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-27 08:11:01 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2258 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/egg/detail/vc7_1_result_of.hpp
+++ boost/egg/detail/vc7_1_result_of.hpp
@@ -11,16 +11,17 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/iteration/iterate.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/preprocessor/seq/size.hpp>
+#include <boost/preprocessor/seq/transform.hpp>
 #include <boost/egg/config.hpp> // BOOST_EGG_MAX_ARITY
-#include <boost/egg/preprocessor/bits_enum.hpp>
 #include <boost/egg/preprocessor/enum_params_with.hpp>
-#include <boost/egg/preprocessor/for_each_bits.hpp>
+#include <boost/egg/preprocessor/seq_enum_i.hpp>
+#include <boost/egg/preprocessor/seq_for_each_product.hpp>
+#include <boost/egg/preprocessor/seq_repeat.hpp>
 #include <boost/egg/result_of.hpp>
-#include <boost/egg/detail/vc7_1_const_ref.hpp>
+#include <boost/egg/detail/bytag_to.hpp>
 
 
 namespace boost { namespace egg { namespace detail_vc7_1 {
@@ -31,25 +32,14 @@ namespace boost { namespace egg { namespace detail_vc7_1 {
 
 
     // supports only lvalue.
-    #define BOOST_EGG_spec(R, Bits, _) \
-        template<class Fun, BOOST_PP_ENUM_PARAMS(BOOST_PP_SEQ_SIZE(Bits), class A)> \
-        struct result_of<Fun, BOOST_EGG_PP_BITS_ENUM_R(R, Bits, BOOST_EGG_match_w0, BOOST_EGG_match_w1, ~)> : \
-            result_of_<Fun(BOOST_EGG_PP_BITS_ENUM_R(R, Bits, BOOST_EGG_fwd_w0, BOOST_EGG_fwd_w1, ~))> \
+    #define BOOST_EGG_spec(R, BytagSeq, Cv) \
+        template<class Fun, BOOST_PP_ENUM_PARAMS(BOOST_PP_SEQ_SIZE(BytagSeq), class A)> \
+        struct result_of<Fun, BOOST_EGG_PP_SEQ_ENUM_I_R(R, BytagSeq, BOOST_EGG_BYTAG_TO_NS_DEDUCED, ~)> : \
+            result_of_<Fun(BOOST_EGG_PP_SEQ_ENUM_I_R(R, BytagSeq, BOOST_EGG_BYTAG_TO_NS_DEDUCED_REBUILT, ~))> \
         { }; \
     /**/
-
-    #define BOOST_EGG_match_w0(R, I, _) BOOST_PP_CAT(A, I) &
-    #define BOOST_EGG_match_w1(R, I, _) const_ref<BOOST_PP_CAT(A, I)>
-    #define BOOST_EGG_fwd_w0(R, I, _) BOOST_PP_CAT(A, I) &
-    #define BOOST_EGG_fwd_w1(R, I, _) BOOST_PP_CAT(A, I) const &
-
         #define  BOOST_PP_ITERATION_PARAMS_1 (3, (1, BOOST_EGG_MAX_ARITY, <boost/egg/detail/vc7_1_result_of.hpp>))
         #include BOOST_PP_ITERATE()
-
-    #undef  BOOST_EGG_fwd_w1
-    #undef  BOOST_EGG_fwd_w0
-    #undef  BOOST_EGG_match_w1
-    #undef  BOOST_EGG_match_w0
     #undef  BOOST_EGG_spec
 
 
@@ -60,8 +50,10 @@ namespace boost { namespace egg { namespace detail_vc7_1 {
 #else
 #define n BOOST_PP_ITERATION()
 
-
-    BOOST_EGG_PP_FOR_EACH_BITS(n, BOOST_EGG_spec, ~)
+    BOOST_EGG_PP_SEQ_FOR_EACH_PRODUCT(
+        BOOST_PP_SEQ_TRANSFORM(BOOST_EGG_BYTAG_TO_SEQ, ~, BOOST_EGG_PP_SEQ_REPEAT(by_perfect, n)), \
+        BOOST_EGG_spec, ~ \
+    )
 
 
 #undef  n
