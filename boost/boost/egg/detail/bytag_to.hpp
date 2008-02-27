922ca33d4b73e02a8f1ca50084316943878488ee
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-02-27 08:11:01 +0000

    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2258 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/boost/egg/detail/bytag_to.hpp
+++ boost/boost/egg/detail/bytag_to.hpp
@@ -43,6 +43,9 @@
     #define BOOST_EGG_BYTAG_TO_DEDUCED_by_value(I) BOOST_PP_CAT(A, I)
 
 
+// For msvc-7.1 bug workaround
+//
+
 #define BOOST_EGG_BYTAG_TO_NS_DEDUCED(R, _, I, Bytag) \
     BOOST_PP_CAT(BOOST_EGG_BYTAG_TO_NS_DEDUCED_, Bytag)(I) \
 /**/
@@ -50,6 +53,15 @@
     #define BOOST_EGG_BYTAG_TO_NS_DEDUCED_by_ref(I) BOOST_PP_CAT(A, I) &
     #define BOOST_EGG_BYTAG_TO_NS_DEDUCED_by_cref(I) BOOST_EGG_NS_CONST_REF(BOOST_PP_CAT(A, I))
     #define BOOST_EGG_BYTAG_TO_NS_DEDUCED_by_value(I) BOOST_PP_CAT(A, I) &
+
+
+#define BOOST_EGG_BYTAG_TO_NS_DEDUCED_REBUILT(R, _, I, Bytag) \
+    BOOST_PP_CAT(BOOST_EGG_BYTAG_TO_NS_DEDUCED_REBUILT_, Bytag)(I) \
+/**/
+
+    #define BOOST_EGG_BYTAG_TO_NS_DEDUCED_REBUILT_by_ref(I) BOOST_PP_CAT(A, I) &
+    #define BOOST_EGG_BYTAG_TO_NS_DEDUCED_REBUILT_by_cref(I) BOOST_PP_CAT(A, I) const &
+    #define BOOST_EGG_BYTAG_TO_NS_DEDUCED_REBUILT_by_value(I) BOOST_PP_CAT(A, I) &
 
 
 #endif
