cf1f7eee4128fabd13bbc313b7afad4b645a6588
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-03-06 22:09:37 +0000

    better doc.
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2369 350e9bb6-6311-0410-90c3-be67731b76ec

--- libs/egg/example/nest.cpp
+++ libs/egg/example/nest.cpp
@@ -33,13 +33,13 @@ void egg_example()
 
     int _6_ = 6, _1_ = 1, _3_ = 3, _9_ = 9;
 
-// Lv: 0     1      2      3      4
-    // \x -> (\y -> (\z -> (\w -> foo(y,z,x,w))))
-    BOOST_CHECK( nest4(foo)(nest1(_1), nest2(_1), nest0(_1), nest3(_1)) /*< Works as `curry4`. >*/
-        (_3_)(_6_)(_1_)(_9_) == foo(6,1,3,9) );
+// Lv: 0     1      2          3
+    // \x -> (\y -> (\(z,w) -> foo(y,w,z,x))))
+    BOOST_CHECK( nest3(foo)(nest1(_1), nest2(_2), nest2(_1), nest0(_1)) /*< `nest0` does nothing, meaning that `nest0(_1)` behaves as `_1`. >*/
+        (_3_)(_6_)(_1_,_9_) == foo(6,9,1,3) );
 
     // \x -> apply(\y -> minus(x,y), plus(x,3))
-    BOOST_CHECK( nest1(apply)(nest2(minus)(nest0(_1), nest1(_1)), nest1(plus)(nest0(_1), 3)) /*< By the definition, `nest1(apply)` has the same semantics as `lazy(apply)`. >*/
+    BOOST_CHECK( nest1(apply)(nest2(minus)(nest0(_1), nest1(_1)), nest1(plus)(nest0(_1), 3)) /*< `nest1(apply)` has the same semantics as `lazy(apply)`. >*/
         (_9_) == minus(9, plus(9,3))  );
 }
 //]
