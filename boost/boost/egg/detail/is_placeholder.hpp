37d66057a8808a59bd4c26cc9585c67d9034d6b7
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-03-09 04:20:14 +0000

    Works under boost1.34.
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2382 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/boost/egg/detail/is_placeholder.hpp
+++ boost/boost/egg/detail/is_placeholder.hpp
@@ -12,7 +12,19 @@
 
 #include <boost/config.hpp> // BOOST_MSVC
 #include <boost/mpl/bool.hpp>
-#include <boost/egg/detail/boost_is_placeholder_fwd.hpp>
+#include <boost/version.hpp>
+
+#if BOOST_VERSION >= 103500
+    #include <boost/is_placeholder.hpp>
+#else
+    #include <boost/lambda/core.hpp>
+#endif
+
+
+namespace boost { namespace egg {
+
+
+#if BOOST_VERSION >= 103500
 
 
 #if defined(BOOST_MSVC)
@@ -20,22 +32,45 @@
     #pragma warning(disable: 4305) // 'specialization' : truncation from 'boost::is_placeholder<T>::_vt' to 'bool'
 #endif
 
-
-namespace boost { namespace egg {
-
-
     template<class T>
     struct is_placeholder_ :
         mpl::bool_<is_placeholder<T>::value>
     { };
 
-
-} } // namespace boost::egg
-
-
 #if defined(BOOST_MSVC)
     #pragma warning(pop)
 #endif
+
+
+#else
+
+
+    template<class T>
+    struct is_placeholder_ :
+        mpl::false_
+    { };
+
+
+    template< >
+    struct is_placeholder_< lambda::lambda_functor< lambda::placeholder<lambda::FIRST> > > :
+        mpl::true_
+    { };
+
+    template< >
+    struct is_placeholder_< lambda::lambda_functor< lambda::placeholder<lambda::SECOND> > > :
+        mpl::true_
+    { };
+
+    template< >
+    struct is_placeholder_< lambda::lambda_functor< lambda::placeholder<lambda::THIRD> > > :
+        mpl::true_
+    { };
+
+
+#endif // BOOST_VERSION >= 103500
+
+
+} } // namespace boost::egg
 
 
 #endif
