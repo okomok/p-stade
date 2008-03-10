d165d2831b90526c4bce9b25dab6e1388f0e1157
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-03-10 17:47:10 +0000

    _N_
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2387 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/egg/nest.hpp
+++ boost/egg/nest.hpp
@@ -1,7 +1,6 @@
 #ifndef BOOST_PP_IS_ITERATING
 #ifndef BOOST_EGG_NEST_HPP
 #define BOOST_EGG_NEST_HPP
-#include <boost/egg/detail/prefix.hpp>
 
 
 // Boost.Egg
@@ -12,31 +11,25 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <boost/mpl/eval_if.hpp>
 #include <boost/preprocessor/arithmetic/dec.hpp>
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/iteration/iterate.hpp>
 #include <boost/egg/bll/bind.hpp>
-#include <boost/egg/bll/protect.hpp>
 #include <boost/egg/by_value.hpp>
 #include <boost/egg/config.hpp> // BOOST_EGG_MAX_NEST_LEVEL
 #include <boost/egg/const.hpp>
-#include <boost/egg/detail/derived_from.hpp>
-#include <boost/egg/detail/enable_if.hpp>
-#include <boost/egg/detail/if_use_default.hpp>
-#include <boost/egg/detail/is_placeholder.hpp>
-#include <boost/egg/detail/nest_impl.hpp>
-#include <boost/egg/detail/unplace.hpp> // inclusion guaranteed
+#include <boost/egg/detail/if_use_default.hpp> // inclusion guaranteed
+#include <boost/egg/detail/nest_placeholder.hpp> // inclusion guaranteed
 #include <boost/egg/identity.hpp>
+#include <boost/egg/lazy.hpp>
 #include <boost/egg/preprocessor/cat3.hpp>
-#include <boost/egg/result_of.hpp>
 
 
 namespace boost { namespace egg {
 
 
 // level0
-    template<class Bind = use_default, class Protect = use_default>
+    template<class Bind = use_default>
     struct X_nest0 :
         X_identity<by_value>
     { };
@@ -53,55 +46,36 @@ namespace boost { namespace egg {
 } } // namespace boost::egg
 
 
-#include <boost/egg/detail/suffix.hpp>
 #endif
 #else
 #define n BOOST_PP_ITERATION()
 
 
     namespace BOOST_EGG_PP_CAT3(nest, n, _detail) {
-
-
-        template<class Bind, class Protect>
-        struct little
+#if n == 1
+        template<class Bind>
+        struct with
         {
-            typedef BOOST_PP_CAT(X_nest, BOOST_PP_DEC(n))<Bind, Protect> S_nestN_1;
-            typedef BOOST_PP_CAT(details::X_nest_impl, n)<Bind> S_nest_implN;
-
-            template<class Me, class Fun>
-            struct apply :
-                mpl::eval_if< is_placeholder_<Fun>,
-                    result_of_<Protect(typename result_of_<S_nestN_1(Fun &)>::type)>,
-                    result_of_<S_nest_implN(Fun &)>
-                >
-            { };
-
-            template<class Re, class Fun>
-            Re call(Fun fun, typename enable_if_< is_placeholder_<Fun> >::type = 0) const
-            {
-                return Protect()(S_nestN_1()(fun));
-            }
-
-            template<class Re, class Fun>
-            Re call(Fun fun, typename disable_if_<is_placeholder_<Fun> >::type = 0) const
-            {
-                return S_nest_implN()(fun);
-            }
+            typedef Bind type;
         };
+#else
+        template<class Bind>
+        struct with :
+            result_of_lazy<
+                Bind,
+                typename BOOST_EGG_PP_CAT3(nest, BOOST_PP_DEC(n), _detail)::with<Bind>::type
+            >
+        { };
+#endif
+    }
 
-
-    } // namespace nestN_detail
-
-
-    template<class Bind = use_default, class Protect = use_default>
-    struct BOOST_PP_CAT(X_nest, n) : details::derived_from <
-        function<
-            BOOST_EGG_PP_CAT3(nest, n, _detail)::little<
-                typename details::if_use_default<Bind, T_bll_bind>::type,
-                typename details::if_use_default<Protect, T_bll_protect>::type
-            >,
-            by_value
-        > >
+    template<class Bind = use_default>
+    struct BOOST_PP_CAT(X_nest, n) :
+        X_lazy<
+            typename BOOST_EGG_PP_CAT3(nest, n, _detail)::with<
+                typename details::if_use_default<Bind, T_bll_bind>::type
+            >::type
+        >
     { };
 
     typedef BOOST_PP_CAT(X_nest, n)<>::base_class BOOST_PP_CAT(T_nest, n);
