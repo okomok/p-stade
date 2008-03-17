e360e3515d5540ae0188a14a11c8e48b9be093c1
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-03-17 20:18:05 +0000

    less instantiations.
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk@2434 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/boost/egg/forward.hpp
+++ boost/boost/egg/forward.hpp
@@ -12,14 +12,14 @@
 
 
 #include <cstddef> // size_t
-#include <boost/mpl/assert.hpp>
+// #include <boost/mpl/assert.hpp>
 #include <boost/preprocessor/array/elem.hpp>
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/enum.hpp>
 #include <boost/egg/detail/adl_barrier.hpp>
 #include <boost/egg/detail/boost_workaround.hpp>
 #include <boost/egg/detail/bytag_at.hpp>
-#include <boost/egg/detail/plain.hpp>
+// #include <boost/egg/detail/plain.hpp>
 #include <boost/egg/function_fwd.hpp> // bytags
 
 
@@ -70,7 +70,7 @@ BOOST_EGG_ADL_BARRIER(forward) { // for C++0x
     template<class Lvalue>
     struct result_of_forward<by_value, Lvalue>
     {
-        BOOST_MPL_ASSERT((details::is_plain<Lvalue>));
+        // BOOST_MPL_ASSERT((details::is_plain<Lvalue>));
         typedef Lvalue type; // For movable types, you can't add const-qualifier.
     };
 
