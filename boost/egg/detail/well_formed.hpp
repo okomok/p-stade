2749aa64f01776ff4dd076fd0b773a04e75b8b94
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-03-22 07:23:41 +0000

    overload<>
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/trunk/boost@2449 350e9bb6-6311-0410-90c3-be67731b76ec

---
+++ boost/egg/detail/well_formed.hpp
@@ -0,0 +1,51 @@
+#ifndef BOOST_PP_IS_ITERATING
+#ifndef BOOST_EGG_DETAIL_WELL_FORMED_HPP
+#define BOOST_EGG_DETAIL_WELL_FORMED_HPP
+
+
+// PStade.Wine
+//
+// Copyright Shunsuke Sogame 2005-2006.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/egg/preprocessor/enum_params_with.hpp>
+#include <boost/egg/config.hpp> // BOOST_EGG_MAX_LINEAR_ARITY
+#include <boost/egg/detail/boost_use_default_fwd.hpp>
+
+
+namespace boost { namespace egg { namespace details {
+
+
+    struct well_formed
+    {
+        typedef use_default result_type; // use_default is useful for NullaryResult.
+
+    // 0ary
+        result_type operator()() const;
+
+    // 1ary-
+        #define BOOST_PP_ITERATION_PARAMS_1 (3, (1, BOOST_EGG_MAX_LINEAR_ARITY, <boost/egg/detail/well_formed.hpp>))
+        #include BOOST_PP_ITERATE()
+    };
+
+
+} } } // namespace boost::egg::details
+
+
+#endif
+#else
+#define n BOOST_PP_ITERATION()
+
+
+template<BOOST_PP_ENUM_PARAMS(n, class A)>
+result_type operator()(BOOST_EGG_PP_ENUM_PARAMS_WITH(n, A, const &)) const;
+
+
+#undef  n
+#endif
