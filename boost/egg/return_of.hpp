b5ead274ba046cdfb984225732a3e5e61ce98b90
mb2sync <mb2sync@350e9bb6-6311-0410-90c3-be67731b76ec>
2008-04-03 19:05:42 +0000

    portable return_of
    git-svn-id: https://p-stade.svn.sourceforge.net/svnroot/p-stade/branches/egg_devel3@2503 350e9bb6-6311-0410-90c3-be67731b76ec

--- boost/egg/return_of.hpp
+++ boost/egg/return_of.hpp
@@ -12,13 +12,10 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <boost/mpl/eval_if.hpp>
-#include <boost/mpl/identity.hpp>
+#include <boost/preprocessor/arithmetic/inc.hpp>
 #include <boost/preprocessor/iteration/iterate.hpp>
 #include <boost/preprocessor/repetition/enum_trailing_params.hpp>
-#include <boost/type_traits/is_function.hpp>
 #include <boost/type_traits/remove_reference.hpp>
-#include <boost/egg/detail/in_fun_spec.hpp>
 #include <boost/egg/preprocessor/enum_params_with.hpp>
 #include <boost/egg/result_of.hpp>
 
@@ -26,34 +23,29 @@
 namespace boost { namespace egg {
 
 
-    template<class Fun>
+    template<class Fun, BOOST_EGG_PP_ENUM_PARAMS_WITH(BOOST_PP_INC(BOOST_RESULT_OF_NUM_ARGS), class A, = void)>
     struct return_of;
 
 
-    template<class Fun>
-    struct as_arg;
+    namespace return_of_detail {
 
 
-    template<class Arg>
-    struct return_of
-    {
-        typedef Arg type;
-    };
+        // See: "Quick-n-Dirty Type Categorization" at Boost.Proto doc.
 
-    template<class Fun>
-    struct return_of< as_arg<Fun> >
-    {
-        typedef Fun type;
-    };
+        template<class T, class Void = void>
+        struct arg_
+        {
+            typedef T type;
+        };
+
+        template<class T>
+        struct arg_<T, typename T::boost_egg_return_of>
+        {
+            typedef typename T::type type;
+        };
 
 
-    template<class Arg>
-    struct return_of<Arg *> :
-        boost::mpl::eval_if< boost::is_function<Arg>, // if decayed
-            return_of<Arg>,
-            boost::mpl::identity<Arg *>
-        >
-    { };
+    } // namespace return_of_detail
 
 
     #define  BOOST_PP_ITERATION_PARAMS_1 (3, (0, BOOST_RESULT_OF_NUM_ARGS, <boost/egg/return_of.hpp>))
@@ -70,13 +62,15 @@ namespace boost { namespace egg {
 
 
     template<class Fun BOOST_PP_ENUM_TRAILING_PARAMS(n, class A)>
-    struct return_of<Fun(BOOST_EGG_ENUM_PARAMS_IN_FUN_SPEC(n, A))> :
+    struct return_of<Fun BOOST_PP_ENUM_TRAILING_PARAMS(n, A)> :
         result_of_<
             typename boost::remove_reference<Fun>::type(
-                BOOST_EGG_PP_ENUM_PARAMS_WITH(n, typename return_of<A, >::type)
+                BOOST_EGG_PP_ENUM_PARAMS_WITH(n, typename return_of_detail::arg_<A, >::type)
             )
         >
-    { };
+    {
+        typedef void boost_egg_return_of;
+    };
 
 
 #undef  n
